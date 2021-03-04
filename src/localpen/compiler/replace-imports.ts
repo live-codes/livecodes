e58cd28179cca2e6935c539c6b925ef037fa8700
Hatem Hosny <hatemhosny@gmail.com>
2021-03-05 00:41:14 +0200

    fix(compilers): fix cannot use import outside module
    avoid using stateful regex

--- src/localpen/compiler/replace-imports.ts
+++ src/localpen/compiler/replace-imports.ts
@@ -3,12 +3,15 @@ import { Pen } from '../models';
 export const importsPattern = /(import\s+?(?:(?:(?:[\w*\s{},\$]*)\s+from\s+?)|))((?:".*?")|(?:'.*?'))([\s]*?(?:;|$|))/g;
 
 export const replaceImports = (code: string, config: Pen) =>
-  code.replace(importsPattern, (statement) => {
-    const libName = statement.replace(importsPattern, '$2').replace(/"/g, '').replace(/'/g, '');
+  code.replace(new RegExp(importsPattern), (statement) => {
+    const libName = statement
+      .replace(new RegExp(importsPattern), '$2')
+      .replace(/"/g, '')
+      .replace(/'/g, '');
     if (libName.startsWith('http') || libName.startsWith('.') || libName.startsWith('/')) {
       return statement;
     }
     const localModule = config.modules.find((module) => module.name === libName);
     const libPath = localModule?.url || 'https://cdn.skypack.dev/' + libName;
-    return statement.replace(importsPattern, `$1'${libPath}'$3`);
+    return statement.replace(new RegExp(importsPattern), `$1'${libPath}'$3`);
   });
