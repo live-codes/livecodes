cdfbac4ceeed9188b2760ea54c21a894d30b7fd1
Hatem Hosny <hatemhosny@gmail.com>
2021-04-24 02:09:17 +0200

    UI improvement

--- src/localpen/app.ts
+++ src/localpen/app.ts
@@ -1132,7 +1132,7 @@ export const app = async (config: Pen) => {
               const loading = document.createElement('div');
               loading.innerHTML = 'Loading...<br /><br />' + item.title + '';
               loading.className = 'centered';
-              modal.show(loading);
+              modal.show(loading, 'small');
 
               const itemId = (link as HTMLElement).dataset.id || '';
               const savedPen = storage.getItem(itemId)?.pen;
--- src/localpen/languages/create-language-menus.ts
+++ src/localpen/languages/create-language-menus.ts
@@ -75,7 +75,12 @@ export const createLanguageMenus = (
         tooltip.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="8"></line></svg>`;
         const tooltipText = document.createElement('div');
         tooltipText.innerHTML = language.info;
-        eventsManager.addEventListener(tooltip, 'mousedown', () => modal.show(tooltipText), false);
+        eventsManager.addEventListener(
+          tooltip,
+          'mousedown',
+          () => modal.show(tooltipText, 'small'),
+          false,
+        );
         languageItem.appendChild(tooltip);
       }
     });
