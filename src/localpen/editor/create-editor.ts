e151b2cdec184c067b9cd7af719a291151926f4c
Hatem Hosny <hatemhosny@gmail.com>
2021-05-11 01:52:45 +0200

    feat(editor): fallback to codemirror on-error loading the editor

--- src/localpen/editor/create-editor.ts
+++ src/localpen/editor/create-editor.ts
@@ -1,10 +1,32 @@
 import { CodeEditor, EditorOptions } from '../models';
 import { isMobile } from '../utils';
 
+const loadEditor = async (
+  editorName: 'monaco' | 'codemirror' | 'prism' | '' | undefined,
+  options: EditorOptions,
+) => {
+  const { baseUrl } = options;
+  const editorUrl = baseUrl + editorName + '.js';
+
+  let editorModule = (window as any)[editorUrl];
+  try {
+    if (!editorModule) {
+      editorModule = await import(editorUrl);
+      (window as any)[editorUrl] = editorModule;
+    }
+    const createCodeEditor: (options: EditorOptions) => Promise<CodeEditor> =
+      editorModule.createEditor;
+    const codeEditor = await createCodeEditor(options);
+    return codeEditor;
+  } catch (err) {
+    return false;
+  }
+};
+
 export const createEditor = async (options: EditorOptions) => {
   if (!options) throw new Error();
 
-  const { baseUrl, editor, mode } = options;
+  const { editor, mode } = options;
 
   const editorName = ['codemirror', 'monaco', 'prism'].includes(editor || '')
     ? editor
@@ -13,20 +35,13 @@ export const createEditor = async (options: EditorOptions) => {
     : isMobile()
     ? 'codemirror'
     : 'monaco';
-  const editorUrl = baseUrl + editorName + '.js';
 
-  let editorModule = (window as any)[editorUrl];
-  if (!editorModule) {
-    editorModule = await import(editorUrl);
-    (window as any)[editorUrl] = editorModule;
-  }
-  try {
-    const createCodeEditor: (options: EditorOptions) => Promise<CodeEditor> =
-      editorModule.createEditor;
+  const codeEditor =
+    (await loadEditor(editorName, options)) || (await loadEditor('codemirror', options));
 
-    const codeEditor = await createCodeEditor(options);
+  if (codeEditor) {
     return codeEditor;
-  } catch (err) {
+  } else {
     throw new Error('Failed loading code editor');
   }
 };
