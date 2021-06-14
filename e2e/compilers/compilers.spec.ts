b6ee956e93e4d5e0468eb4052ea71e1e2afdbefc
Hatem Hosny <hatemhosny@gmail.com>
2021-06-14 14:29:23 +0200

    test(compilers): add e2e tests for markup compilers

--- e2e/compilers/compilers.spec.ts
+++ e2e/compilers/compilers.spec.ts
@@ -46,6 +46,30 @@ test.describe('Compiler Results', () => {
     expect(resultText).toBe('Hi There');
   });
 
+  test('MDX', async ({ page }) => {
+    await page.goto(getTestUrl());
+
+    const { app, getResult, waitForResultUpdate } = await getLoadedApp(page);
+
+    await app.click(':nth-match([title="change language"], 1)');
+    await app.click('text=MDX');
+    await waitForEditorFocus(app);
+    await app.page().keyboard.type('<Hello title="World" />');
+
+    await app.click(':nth-match([title="change language"], 3)');
+    await app.click('text=JSX');
+    await waitForEditorFocus(app);
+    await app
+      .page()
+      .keyboard.type('export const Hello = (props) => <h1>Hello, {props.title}!</h1>;');
+
+    await app.click(runButtonSelector);
+    await waitForResultUpdate();
+    const resultText = await getResult().innerHTML('h1');
+
+    expect(resultText).toBe('Hello, World!');
+  });
+
   test('Pug', async ({ page }) => {
     await page.goto(getTestUrl());
 
@@ -61,5 +85,39 @@ test.describe('Compiler Results', () => {
     const resultText = await getResult().innerHTML('h1');
 
     expect(resultText).toBe('Hi There');
+  });
+
+  test('Haml', async ({ page }) => {
+    await page.goto(getTestUrl());
+
+    const { app, getResult, waitForResultUpdate } = await getLoadedApp(page);
+
+    await app.click(':nth-match([title="change language"], 1)');
+    await app.click('text=Haml');
+    await waitForEditorFocus(app);
+    await page.keyboard.type('.content Hello, World!');
+
+    await app.click(runButtonSelector);
+    await waitForResultUpdate();
+    const resultText = await getResult().innerHTML('.content');
+
+    expect(resultText).toContain('Hello, World!');
+  });
+
+  test('AsciiDoc', async ({ page }) => {
+    await page.goto(getTestUrl());
+
+    const { app, getResult, waitForResultUpdate } = await getLoadedApp(page);
+
+    await app.click(':nth-match([title="change language"], 1)');
+    await app.click('text=AsciiDoc');
+    await waitForEditorFocus(app);
+    await page.keyboard.type('== Hello, World!');
+
+    await app.click(runButtonSelector);
+    await waitForResultUpdate();
+    const resultText = await getResult().innerHTML('h2');
+
+    expect(resultText).toContain('Hello, World!');
   });
 });
