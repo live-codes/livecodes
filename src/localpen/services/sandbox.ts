95c4a75f79bd02a95a7a1ed8c1af035dbb330939
Hatem Hosny <hatemhosny@gmail.com>
2021-08-06 08:38:03 +0200

    use blob worker

--- src/localpen/services/sandbox.ts
+++ src/localpen/services/sandbox.ts
@@ -1,22 +1,5 @@
-const local = (url: string) => {
-  if (
-    window.location.origin.startsWith('http://127.0.0.1') ||
-    window.location.origin.startsWith('http://localhost')
-  ) {
-    if (url === 'result') {
-      return '/result.html';
-    }
-    if (url === 'compiler') {
-      return '/compiler.html';
-    }
-    if (url === 'origin') {
-      return window.location.origin;
-    }
-  }
-  return false;
-};
 export const sandboxService = {
-  getResultUrl: () => local('result') || 'https://localpen-sandbox.pages.dev/v1/result',
-  getCompilerUrl: () => local('compiler') || 'https://localpen-sandbox.pages.dev/v1/compiler',
-  getOrigin: () => local('origin') || 'https://localpen-sandbox.pages.dev',
+  getResultUrl: () => 'https://localpen-sandbox.pages.dev/v1/result',
+  getCompilerUrl: () => 'https://localpen-sandbox.pages.dev/v1/compiler',
+  getOrigin: () => 'https://localpen-sandbox.pages.dev',
 };
