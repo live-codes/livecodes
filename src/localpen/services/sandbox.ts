9c9dc4d132cf500bfe04974f1f1d9d7c4d994977
Hatem Hosny <hatemhosny@gmail.com>
2021-08-22 01:07:08 +0200

    v2 sandbox

--- src/localpen/services/sandbox.ts
+++ src/localpen/services/sandbox.ts
@@ -1,10 +1,5 @@
-// export const sandboxService = {
-//   getResultUrl: () => 'https://localpen-sandbox.pages.dev/v1/result',
-//   getCompilerUrl: () => 'https://localpen-sandbox.pages.dev/v1/compiler',
-//   getOrigin: () => 'https://localpen-sandbox.pages.dev',
-// };
 export const sandboxService = {
-  getResultUrl: () => '/result.html',
-  getCompilerUrl: () => '/compiler.html',
-  getOrigin: () => location.origin,
+  getResultUrl: () => 'https://localpen-sandbox.pages.dev/v2/result',
+  getCompilerUrl: () => 'https://localpen-sandbox.pages.dev/v2/compiler',
+  getOrigin: () => 'https://localpen-sandbox.pages.dev',
 };
