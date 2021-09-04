9493224becf40d80735ceb8fdfa50f9b0d0f2fee
Hatem Hosny <hatemhosny@gmail.com>
2021-09-04 12:29:11 +0200

    fix(compiled): fix compiled code viewer going out of sync for dynamically changed code from result page

--- src/localpen/app.ts
+++ src/localpen/app.ts
@@ -45,7 +45,14 @@ import { getStarterTemplates } from './templates';
 import { defaultConfig, getConfig, setConfig, upgradeAndValidate } from './config';
 import { createToolsPane, createConsole, createCompiledCodeViewer } from './toolspane';
 import { importCode } from './import';
-import { compress, copyToClipboard, debounce, stringify, stringToValidJson } from './utils';
+import {
+  compress,
+  copyToClipboard,
+  debounce,
+  objectMap,
+  stringify,
+  stringToValidJson,
+} from './utils';
 import { getCompiler, getAllCompilers } from './compiler';
 import { createTypeLoader } from './types';
 import { createResultPage } from './result';
@@ -69,11 +76,17 @@ export const app = async (config: Readonly<Pen>, baseUrl: string) => {
   let changingContent = false;
   let toolsPane: any;
   let resultLanguages: Language[] = [];
-  let lastCompiled: { [key in EditorId]: string } | undefined;
   let consoleInputCodeCompletion: any;
   let starterTemplates: Template[];
   let authService: ReturnType<typeof createAuthService> | undefined;
   const screens: Screen[] = [];
+  let cache: {
+    [key in EditorId]: { language: Language; content: string; compiled: string; modified: string };
+  } = {
+    markup: { language: 'html', content: '', compiled: '', modified: '' },
+    style: { language: 'css', content: '', compiled: '', modified: '' },
+    script: { language: 'javascript', content: '', compiled: '', modified: '' },
+  };
 
   const split = UI.createSplitPanes();
 
@@ -424,7 +437,7 @@ export const app = async (config: Readonly<Pen>, baseUrl: string) => {
     });
   };
 
-  const updateCompiledCode = (lastCompilation = lastCompiled) => {
+  const updateCompiledCode = () => {
     const getCompiledLanguage = (editorId: EditorId) => {
       const defaultLang: { [key in EditorId]: Language } = {
         markup: 'html',
@@ -442,10 +455,10 @@ export const app = async (config: Readonly<Pen>, baseUrl: string) => {
       style: getCompiledLanguage('style'),
       script: getCompiledLanguage('script'),
     };
-    if (toolsPane && toolsPane.compiled && lastCompilation) {
-      Object.keys(lastCompilation).forEach((editorId) => {
+    if (toolsPane && toolsPane.compiled) {
+      Object.keys(cache).forEach((editorId) => {
         if (editorId !== getConfig().activeEditor) return;
-        let compiledCode = lastCompilation?.[editorId] || '';
+        let compiledCode = cache[editorId].modified || cache[editorId].compiled || '';
         if (editorId === 'script' && editors.script.getLanguage() === 'php') {
           compiledCode = phpHelper({ code: compiledCode }) || '<?php\n';
         }
@@ -480,29 +493,28 @@ export const app = async (config: Readonly<Pen>, baseUrl: string) => {
       compiler.compile(scriptContent, scriptLanguage, config),
     ]);
 
-    // cache compiled code
-    lastCompiled = {
-      markup: compiledMarkup,
-      style: compiledStyle,
-      script: compiledScript,
-    };
-
-    const compiledCode = {
+    cache = {
       markup: {
         language: markupLanguage,
-        content: compiledMarkup,
+        content: markupContent,
+        compiled: compiledMarkup,
+        modified: compiledMarkup === cache.markup.compiled ? cache.markup.modified : '',
       },
       style: {
         language: styleLanguage,
-        content: compiledStyle,
+        content: styleContent,
+        compiled: compiledStyle,
+        modified: compiledStyle === cache.style.compiled ? cache.style.modified : '',
       },
       script: {
         language: scriptLanguage,
-        content: compiledScript,
+        content: scriptContent,
+        compiled: compiledScript,
+        modified: compiledScript === cache.script.compiled ? cache.script.modified : '',
       },
     };
 
-    return createResultPage(compiledCode, config, forExport, template, baseUrl, singleFile);
+    return createResultPage(cache, config, forExport, template, baseUrl, singleFile);
   };
 
   const setLoading = (status: boolean) => {
@@ -648,7 +660,7 @@ export const app = async (config: Readonly<Pen>, baseUrl: string) => {
     setWindowTitle();
 
     // reset url params
-    updateUrl(url || location.origin + location.pathname);
+    updateUrl(url || location.origin + location.pathname, true);
 
     // load config
     await bootstrap(true);
@@ -1563,8 +1575,8 @@ export const app = async (config: Readonly<Pen>, baseUrl: string) => {
             config: getContentConfig(getConfig()),
             content: {
               resultPage: resultHtml,
-              style: lastCompiled?.style || '',
-              script: lastCompiled?.script || '',
+              style: cache.style.compiled || '',
+              script: cache.script.compiled || '',
             },
             message,
             commitSource,
@@ -1788,14 +1800,10 @@ export const app = async (config: Readonly<Pen>, baseUrl: string) => {
           event.data.payload &&
           getEditorLanguages().includes(event.data.payload.language)
         ) {
-          if (lastCompiled) {
-            const editorId = getLanguageEditorId(event.data.payload.language);
-            if (!editorId) return;
-            updateCompiledCode({
-              ...lastCompiled,
-              [editorId]: event.data.payload.content || '',
-            });
-          }
+          const editorId = getLanguageEditorId(event.data.payload.language);
+          if (!editorId) return;
+          cache[editorId].modified = event.data.payload.content || '';
+          updateCompiledCode();
         }
       });
     };
