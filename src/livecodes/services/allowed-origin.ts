0fc2230f863bc4760a62cf46dd94433a27149cea
Hatem Hosny <hatemhosny@gmail.com>
2021-09-19 00:34:51 +0200

    feat(services): add cors service

--- src/livecodes/services/allowed-origin.ts
+++ src/livecodes/services/allowed-origin.ts
@@ -2,5 +2,12 @@ export const allowedOrigin = (origin = location.origin) =>
   origin &&
   (origin.endsWith('livecodes.io') ||
     origin.endsWith('livecodes.pages.dev') ||
+    origin.endsWith('localpen.io') ||
+    origin.endsWith('localpen.pages.dev') ||
     origin.startsWith('http://127.0.0.1') ||
     origin.startsWith('http://localhost'));
+
+export const allowedTarget = (url: string) =>
+  new RegExp(/^(?:(?:http|https):\/\/(?:\w+.)?)(githubusercontent.com|jsbin.com|)\/(?:.*)/g).test(
+    url,
+  );
