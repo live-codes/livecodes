57a9b0a5df3d85a3ae8f3d966e8e9da5850396b7
Hatem Hosny <hatemhosny@gmail.com>
2021-09-30 04:45:28 +0200

    fix: improve restore state

--- src/livecodes/app.ts
+++ src/livecodes/app.ts
@@ -84,7 +84,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
   const storage = createStorage();
   const templates = createStorage('__livecodes_templates__');
   const userConfigStore = createStorage('__livecodes_user_config__');
-  const unsavedProjectRestore = createStorage('__livecodes_project_restore__');
+  const projectRestore = createStorage('__livecodes_project_restore__');
   const formatter = getFormatter(getConfig(), baseUrl);
   const notifications = createNotifications();
   const modal = createModal();
@@ -117,9 +117,9 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
 
       const scriptLang = getEditorLanguage('script') || 'javascript';
       const compilers = getAllCompilers(languages, getConfig(), baseUrl);
-      const editorsText = `${editors?.markup.getValue()}
-      ${editors?.style.getValue()}
-      ${editors?.script.getValue()}
+      const editorsText = `${getConfig().markup.content}
+      ${getConfig().style.content}
+      ${getConfig().script.content}
       `;
       const liveReload =
         compilers[scriptLang]?.liveReload &&
@@ -195,7 +195,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
         ...getLanguageCompiler(scriptLanguage)?.types,
         ...config.types,
       };
-      const libs = await typeLoader.load(editors.script.getValue(), configTypes);
+      const libs = await typeLoader.load(getConfig().script.content || '', configTypes);
       libs.forEach((lib) => editors.script.addTypes?.(lib));
     }
   };
@@ -207,7 +207,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
       languages.find((language) => language.name === getLanguageByAlias(title))?.title || '';
   };
 
-  const createCopyButtons = (editors: Editors) => {
+  const createCopyButtons = () => {
     const editorIds: EditorId[] = ['markup', 'style', 'script'];
     editorIds.forEach((editorId) => {
       const copyButton = document.createElement('button');
@@ -215,7 +215,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
       copyButton.classList.add('copy-button');
       document.getElementById(editorId)?.appendChild(copyButton);
       eventsManager.addEventListener(copyButton, 'click', () => {
-        if (copyToClipboard(editors[editorId].getValue())) {
+        if (copyToClipboard(editors?.[editorId]?.getValue())) {
           copyButton.innerHTML = 'copied';
           setTimeout(() => {
             copyButton.innerHTML = 'copy';
@@ -286,7 +286,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
     });
 
     if (config.mode === 'codeblock') {
-      createCopyButtons(editors);
+      createCopyButtons();
     }
 
     return editors;
@@ -369,20 +369,17 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
     titles.forEach((selector) => selector.classList.remove('active'));
     const activeTitle = document.getElementById(editorId + '-selector');
     activeTitle?.classList.add('active');
-
     const editorDivs = UI.getEditorDivs();
     editorDivs.forEach((editor) => (editor.style.display = 'none'));
     const activeEditor = document.getElementById(editorId) as HTMLElement;
     activeEditor.style.display = 'block';
     editors[editorId].focus();
-
     if (!isUpdate) {
       setConfig({
         ...getConfig(),
         activeEditor: editorId,
       });
     }
-
     updateCompiledCode();
     split.show('code');
   };
@@ -397,7 +394,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
     ) {
       if (editors.script && typeof editors.script.addTypes === 'function') {
         consoleInputCodeCompletion = editors.script.addTypes({
-          content: editors.script.getValue() + '\nexport {}',
+          content: getConfig().script.content + '\nexport {}',
           filename: 'script.js',
         });
       }
@@ -425,7 +422,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
     const editorId = getLanguageEditorId(language);
     if (!editorId || !language || !languageIsEnabled(language, getConfig())) return;
     const editor = editors[editorId];
-    editor.setLanguage(language, value ?? editor.getValue());
+    editor.setLanguage(language, value ?? (getConfig()[editorId].content || ''));
     if (editorLanguages) {
       editorLanguages[editorId] = language;
     }
@@ -442,6 +439,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
       });
       await run();
     }
+    setSavedStatus();
     addConsoleInputCodeCompletion();
     loadModuleTypes(editors, getConfig());
     applyLanguageConfigs(language);
@@ -462,7 +460,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
         style: 'css',
         script: 'javascript',
       };
-      const lang = getLanguageCompiler(editors[editorId].getLanguage())?.compiledCodeLanguage;
+      const lang = getLanguageCompiler(getConfig()[editorId].language)?.compiledCodeLanguage;
       return {
         language: lang || defaultLang[editorId],
         label: lang === 'json' ? 'JSON' : getLanguageByAlias(lang) || lang || defaultLang[editorId],
@@ -478,7 +476,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
       Object.keys(cache).forEach((editorId) => {
         if (editorId !== getConfig().activeEditor) return;
         let compiledCode = cache[editorId].modified || cache[editorId].compiled || '';
-        if (editorId === 'script' && editors.script.getLanguage() === 'php') {
+        if (editorId === 'script' && getConfig().script.language === 'php') {
           compiledCode = phpHelper({ code: compiledCode }) || '<?php\n';
         }
         toolsPane.compiled.update(
@@ -759,16 +757,16 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
   };
 
   const setProjectRestore = (reset = false) => {
-    unsavedProjectRestore.clear();
+    projectRestore.clear();
     if (reset || !getConfig().enableRestore) return;
-    unsavedProjectRestore.addItem(getContentConfig(getConfig()));
+    projectRestore.addItem(getContentConfig(getConfig()));
   };
 
   const checkRestoreStatus = () => {
     if (!getConfig().enableRestore) {
       return Promise.resolve('restore disabled');
     }
-    const unsavedItem = unsavedProjectRestore.getAllData().pop();
+    const unsavedItem = projectRestore.getAllData().pop();
     const unsavedProject = unsavedItem?.pen;
     if (!unsavedItem || !unsavedProject) {
       return Promise.resolve('no unsaved project');
@@ -1008,6 +1006,7 @@ export const app = async (appConfig: Readonly<Config>, baseUrl: string): Promise
           'click',
           () => {
             showEditor(title.dataset.editor as EditorId);
+            setProjectRestore();
           },
           false,
         );
