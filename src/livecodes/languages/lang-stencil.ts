bcfb526cff13ba2acd3ba0794e945616bf322a92
Hatem Hosny <hatemhosny@gmail.com>
2021-11-05 22:37:52 +0200

    upgrade stencil

--- src/livecodes/languages/lang-stencil.ts
+++ src/livecodes/languages/lang-stencil.ts
@@ -3,7 +3,7 @@ import { vendorsBaseUrl } from '../vendors';
 import { parserPlugins } from './prettier';
 import { getLanguageCustomSettings } from './utils';
 
-const cdnUrl = 'https://cdn.jsdelivr.net/npm/@stencil/core@2.8.1/compiler/stencil.min.js';
+const cdnUrl = 'https://cdn.jsdelivr.net/npm/@stencil/core@2.10.0/compiler/stencil.min.js';
 
 export const stencil: LanguageSpecs = {
   name: 'stencil',
@@ -16,7 +16,10 @@ export const stencil: LanguageSpecs = {
     url: cdnUrl,
     factory: () => async (code, { config }) => {
       const result = await (window as any).stencil.transpile(code, {
+        // TranspileOptions interface
+        // https://github.com/ionic-team/stencil/blob/1b8b7ec21f2622d05c9aafa417b2abdd4f2597a4/src/declarations/stencil-public-compiler.ts#L2311
         sourceMap: false,
+        target: 'es2019',
         ...getLanguageCustomSettings('stencil', config),
       });
       return result.code;
