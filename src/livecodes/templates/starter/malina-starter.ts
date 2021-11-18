c281d4dcd30ba60bf37a6ef32d629e7004b65715
Hatem Hosny <hatemhosny@gmail.com>
2021-11-18 22:36:52 +0200

    add malinajs starter template

---
+++ src/livecodes/templates/starter/malina-starter.ts
@@ -0,0 +1,51 @@
+import { Template } from '../../models';
+
+export const malinaStarter: Template = {
+  name: 'malina',
+  title: 'Malina.js Starter',
+  thumbnail: 'assets/templates/malina.svg',
+  activeEditor: 'script',
+  markup: {
+    language: 'html',
+    content: '',
+  },
+  style: {
+    language: 'css',
+    content: '',
+  },
+  script: {
+    language: 'malina',
+    content: `
+<script>
+  let title = "Malina.js";
+  let counter = 0;
+  function increment() {
+    counter += 1;
+  }
+</script>
+
+<style>
+  .container,
+  .container button {
+    text-align: center;
+    font: 1em sans-serif;
+  }
+  .logo {
+    width: 150px;
+  }
+</style>
+
+<div class="container">
+  <h1>Hello, {title}!</h1>
+  <img class="logo" src="{{ __livecodes_baseUrl__ }}assets/templates/malina.svg" />
+  <p>You clicked {counter} times.</p>
+  <button on:click="{increment}">Click me</button>
+</div>
+`.trimStart(),
+  },
+  stylesheets: [],
+  scripts: [],
+  cssPreset: '',
+  imports: {},
+  types: {},
+};
