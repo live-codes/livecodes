9b979fc3b8fb1a0313d0613576c29cd027b566cf
Hatem Hosny <hatemhosny@gmail.com>
2021-12-28 01:09:20 +0200

    minor edits

--- src/livecodes/languages/lang-tcl.ts
+++ src/livecodes/languages/lang-tcl.ts
@@ -14,33 +14,35 @@ export const tcl: LanguageSpecs = {
     inlineScript: `
 livecodes.tcl = livecodes.tcl || {};
 livecodes.tcl.run = livecodes.tcl.run || ((input) => new Promise(async (resolve) => {
-  const runCode = async (code, interp) => {
+  livecodes.tcl.input = input;
+  livecodes.tcl.output = null;
+
+  const runCode = async (code, input, interp) => {
     const originalPrompt = window.prompt;
-    window.prompt = () => livecodes.tcl.input;
+    window.prompt = () => input;
     let output = null;
     let error = null;
-    interp.stdout = val => (output = (output ?? '') + val + ('\\n'));
-    interp.stderr = val => (error = (error ?? '') + val + ('\\n'));
+    interp.stdout = val => {output = (output ?? '') + val + '\\n'};
+    interp.stderr = val => {error = (error ?? '') + val + '\\n'};
     const value = interp.Eval(code);
     output = (output ?? '') + value;
     window.prompt = originalPrompt;
     return {output, error};
   };
+
   let code = '';
-  livecodes.tcl.input = input;
-  livecodes.tcl.output = null;
   const scripts = document.querySelectorAll('script[type="text/tcl"]');
   scripts.forEach(script => code += script.innerHTML + '\\n');
 
   const interpreter = await livecodes.tcl.loaded;
-  const result = await runCode(code, interpreter);
+  const result = await runCode(code, input, interpreter);
 
   if (result.error != null) {
     console.error(result.error);
   } else if (result.output != null) {
     console.log(result.output);
   }
-  livecodes.tcl.input = result.input;
+  livecodes.tcl.input = input;
   livecodes.tcl.output = result.output;
   livecodes.tcl.error = result.error;
   livecodes.tcl.ready = true;
@@ -55,8 +57,8 @@ livecodes.tcl.loaded = new Promise(resolve => {
   });
 });
 window.addEventListener('load', async () => {
-  livecodes.tcl.ready = false;
   parent.postMessage({ type: 'loading', payload: true }, '*');
+  livecodes.tcl.ready = false;
   await livecodes.tcl.run(livecodes.tcl.input);
   parent.postMessage({ type: 'loading', payload: false }, '*');
 });
