426369ba37235495af4de9dfd49c63b1cfd9f3bb
Hatem Hosny <hatemhosny@gmail.com>
2022-02-10 14:46:00 +0200

    fix stylus

--- src/livecodes/languages/lang-stylus.ts
+++ src/livecodes/languages/lang-stylus.ts
@@ -5,8 +5,8 @@ export const stylus: LanguageSpecs = {
   name: 'stylus',
   title: 'Stylus',
   compiler: {
-    url: vendorsBaseUrl + 'stylus/stylus.js',
-    factory: () => async (code) => (window as any).stylus.default.render(code),
+    url: vendorsBaseUrl + 'stylus/stylus.min.js',
+    factory: () => async (code) => (window as any).stylus.render(code),
   },
   extensions: ['styl'],
   editor: 'style',
