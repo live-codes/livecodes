f1157b278dc15233dbb0d32ebfaac6ebf59b4973
Hatem Hosny <hatemhosny@gmail.com>
2022-02-26 02:45:08 +0200

    headers

--- src/_worker.js
+++ src/_worker.js
@@ -2,8 +2,9 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     const response = env.ASSETS.fetch(request);
-    // response.headers.append('x-workers-hello', 'Hello from Cloudflare Workers');
-    // response.headers.append('x-myurl', url.href);
-    return response;
+    const newResponse = new Response(response.body, response);
+    newResponse.headers.append('x-workers-hello', 'Hello from Cloudflare Workers');
+    newResponse.headers.append('x-myurl', url.href);
+    return newResponse;
   },
 };
