f4ad5d36ec4fe1e3506308f8d522286ac28a21d2
Hatem Hosny <hatemhosny@gmail.com>
2022-02-26 04:55:45 +0200

    encode url

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,8 @@ export async function onRequest(context) {
   const url = new URL(request.url);
   const res = await env.ASSETS.fetch(request);
   const response = new Response(res.body, res);
-  const linkHeader = `<https://api.livecodes.io/oembed?url=${url.href}&format=json>; rel="alternate"; type="application/json+oembed"; title="LiveCodes oEmbed"`;
+  const oembedUrl = encodeURIComponent(url.href);
+  const linkHeader = `<https://api.livecodes.io/oembed?url=${oembedUrl}&format=json>; rel="alternate"; type="application/json+oembed"; title="LiveCodes oEmbed"`;
   response.headers.append('Link', linkHeader);
   return response;
 }
