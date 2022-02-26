b17c649c9197df80989d110c03b56e03003bdeef
Hatem Hosny <hatemhosny@gmail.com>
2022-02-26 12:22:40 +0200

    add oembed link tag

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 export async function onRequest(context) {
   const { request, env } = context;
   const url = new URL(request.url);
-  const res = await env.ASSETS.fetch(request);
-  const response = new Response(res.body, res);
   const oembedUrl = encodeURIComponent(url.href);
+  const originalResponse = await env.ASSETS.fetch(request);
+  const modifiedBody = originalResponse
+    .text()
+    .replace(
+      'href="https://api.livecodes.io/oembed?url=https%3A%2F%2Flivecodes.io&format=json"',
+      `href="https://api.livecodes.io/oembed?url=${oembedUrl}&format=json"`,
+    );
+  const response = new Response(modifiedBody, originalResponse);
   const linkHeader = `<https://api.livecodes.io/oembed?url=${oembedUrl}&format=json>; rel="alternate"; type="application/json+oembed"; title="LiveCodes oEmbed"`;
   response.headers.append('Link', linkHeader);
   return response;
