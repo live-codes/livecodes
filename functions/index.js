4aea4456ce9023529ba897ccc2b7ccc0e933fcd8
Hatem Hosny <hatemhosny@gmail.com>
2022-02-26 03:49:07 +0200

    edit

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,4 @@
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
+export async function onRequest(request) {
   const url = new URL(request.url);
   const response = await fetch(request);
   const newResponse = new Response(response.body, response);
