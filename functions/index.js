6596595555e1d45d07f598db7aab755b6ead6cd0
Hatem Hosny <hatemhosny@gmail.com>
2022-02-26 04:54:09 +0200

    feat(functions): add link header for oembed

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 export async function onRequest(context) {
   const { request, env } = context;
   const url = new URL(request.url);
-  const response = await env.ASSETS.fetch(request);
-  const newResponse = new Response(response.body, response);
-  newResponse.headers.append('x-workers-hello', 'Hello from Cloudflare Workers');
-  newResponse.headers.append('x-myurl', url.href);
-  return newResponse;
+  const res = await env.ASSETS.fetch(request);
+  const response = new Response(res.body, res);
+  const linkHeader = `<https://api.livecodes.io/oembed?url=${url.href}&format=json>; rel="alternate"; type="application/json+oembed"; title="LiveCodes oEmbed"`;
+  response.headers.append('Link', linkHeader);
+  return response;
 }
