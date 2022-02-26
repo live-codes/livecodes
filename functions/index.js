e7170ce1182a33990063cee07511d073a17d5176
Hatem Hosny <hatemhosny@gmail.com>
2022-02-26 12:36:13 +0200

    fix

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,10 @@ export async function onRequest(context) {
   const url = new URL(request.url);
   const oembedUrl = encodeURIComponent(url.href);
   const originalResponse = await env.ASSETS.fetch(request);
-  const modifiedBody = originalResponse
-    .text()
-    .replace(
-      'href="https://api.livecodes.io/oembed?url=https%3A%2F%2Flivecodes.io&format=json"',
-      `href="https://api.livecodes.io/oembed?url=${oembedUrl}&format=json"`,
-    );
+  const modifiedBody = (await originalResponse.text()).replace(
+    'href="https://api.livecodes.io/oembed?url=https%3A%2F%2Flivecodes.io&format=json"',
+    `href="https://api.livecodes.io/oembed?url=${oembedUrl}&format=json"`,
+  );
   const response = new Response(modifiedBody, originalResponse);
   const linkHeader = `<https://api.livecodes.io/oembed?url=${oembedUrl}&format=json>; rel="alternate"; type="application/json+oembed"; title="LiveCodes oEmbed"`;
   response.headers.append('Link', linkHeader);
