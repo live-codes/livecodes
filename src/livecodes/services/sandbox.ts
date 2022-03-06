5b77c9910c4e1bca0796a76fc72683e427128a11
Hatem Hosny <hatemhosny@gmail.com>
2022-03-06 20:36:31 +0200

    sandbox service

--- src/livecodes/services/sandbox.ts
+++ src/livecodes/services/sandbox.ts
@@ -1,5 +1,11 @@
+const cfPagesBaseUrl = 'https://livecodes-sandbox.pages.dev';
+const ghPagesBaseUrl = 'https://live-codes.github.io/livecodes-sandbox/dist';
+
+const serviceBaseUrl = process.env.CI ? ghPagesBaseUrl : cfPagesBaseUrl;
+const version = 'v4';
+
 export const sandboxService = {
-  getResultUrl: () => 'https://livecodes-sandbox.pages.dev/v4/result',
-  getCompilerUrl: () => 'https://livecodes-sandbox.pages.dev/v4/compiler',
-  getOrigin: () => 'https://livecodes-sandbox.pages.dev',
+  getResultUrl: () => `${serviceBaseUrl}/${version}/result`,
+  getCompilerUrl: () => `${serviceBaseUrl}/${version}/compiler`,
+  getOrigin: () => new URL(serviceBaseUrl).origin,
 };
