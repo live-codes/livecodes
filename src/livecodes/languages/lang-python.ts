61fd5140e6aaa4deb55a2505a64dd7a239a3f358
Hatem Hosny <hatemhosny@gmail.com>
2022-05-21 07:44:08 +0200

    feat(compiled): show python (brython) compiled code

--- src/livecodes/languages/lang-python.ts
+++ src/livecodes/languages/lang-python.ts
@@ -14,12 +14,34 @@ export const python: LanguageSpecs = {
       const { autoloadStdlib, ...options } = getLanguageCustomSettings('python', config);
       const importsPattern = /^(?:from[ ]+(\S+)[ ]+)?import[ ]+(\S+)(?:[ ]+as[ ]+\S+)?[ ]*$/gm;
       const stdlib = autoloadStdlib !== false && compiled.match(importsPattern) ? [stdlibUrl] : [];
+      const compiledCode = `window.addEventListener("load", async () => {
+        const script = document.createElement('script');
+        script.type = 'text/python';
+        script.innerHTML = \`
+from javascript import py2js
+from browser import window
+window.livecodes.compiled = py2js("""${compiled.replace(/\"\"\"/gm, '\\\\"\\\\"\\\\"')}""")
+        \`;
+        document.body.append(script);
+
+        const send = () => {
+          parent.postMessage({type: "compiled", payload: {language: "python", content: window.livecodes.compiled}}, "*");
+          window.livecodes.compiled = '';
+        }
+        const delay = (duration) => new Promise((resolve) => {
+          setTimeout(resolve, duration);
+        });
+        while (!window.livecodes.compiled) {
+          await delay(100);
+        }
+        send();
+      });`;
+      const compiledCodeUrl = 'data:text/plain;base64,' + btoa(compiledCode);
       const loader = `window.addEventListener("load", () => {brython(${JSON.stringify(options)})})`;
       const loaderUrl = 'data:text/plain;base64,' + btoa(loader);
-      return [brythonUrl, ...stdlib, loaderUrl];
+      return [brythonUrl, ...stdlib, compiledCodeUrl, loaderUrl];
     },
     scriptType: 'text/python',
-    compiledCodeLanguage: 'python',
   },
   extensions: ['py'],
   editor: 'script',
