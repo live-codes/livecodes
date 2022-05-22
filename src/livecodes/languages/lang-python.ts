7a8a8c23b076f723861cd7348501c9fa88d53a06
Hatem Hosny <hatemhosny@gmail.com>
2022-05-22 12:28:46 +0200

    fix Python compiler failing on generating compiled code

--- src/livecodes/languages/lang-python.ts
+++ src/livecodes/languages/lang-python.ts
@@ -1,6 +1,6 @@
 import { LanguageSpecs } from '../models';
 import { brythonBaseUrl } from '../vendors';
-import { getLanguageCustomSettings } from './utils';
+import { escapeCode, getLanguageCustomSettings } from './utils';
 
 const brythonUrl = brythonBaseUrl + 'brython.min.js';
 const stdlibUrl = brythonBaseUrl + 'brython_stdlib.js';
@@ -14,32 +14,14 @@ export const python: LanguageSpecs = {
       const { autoloadStdlib, ...options } = getLanguageCustomSettings('python', config);
       const importsPattern = /^(?:from[ ]+(\S+)[ ]+)?import[ ]+(\S+)(?:[ ]+as[ ]+\S+)?[ ]*$/gm;
       const stdlib = autoloadStdlib !== false && compiled.match(importsPattern) ? [stdlibUrl] : [];
-      const compiledCode = `window.addEventListener("load", async () => {
-        const script = document.createElement('script');
-        script.type = 'text/python';
-        script.innerHTML = \`
-from javascript import py2js
-from browser import window
-window.livecodes.compiled = py2js("""${compiled.replace(/\"\"\"/gm, '\\\\"\\\\"\\\\"')}""")
-        \`;
-        document.body.append(script);
-
-        const send = () => {
-          parent.postMessage({type: "compiled", payload: {language: "python", content: window.livecodes.compiled}}, "*");
-          window.livecodes.compiled = '';
-        }
-        const delay = (duration) => new Promise((resolve) => {
-          setTimeout(resolve, duration);
-        });
-        while (!window.livecodes.compiled) {
-          await delay(100);
-        }
-        send();
-      });`;
-      const compiledCodeUrl = 'data:text/plain;base64,' + btoa(compiledCode);
       const loader = `window.addEventListener("load", () => {brython(${JSON.stringify(options)})})`;
       const loaderUrl = 'data:text/plain;base64,' + btoa(loader);
-      return [brythonUrl, ...stdlib, compiledCodeUrl, loaderUrl];
+      const compiledCode = `window.addEventListener("load", async () => {
+        const content = __BRYTHON__.python_to_js(\`${escapeCode(compiled)}\`);
+        parent.postMessage({type: "compiled", payload: {language: "python", content}}, "*");
+      });`;
+      const compiledCodeUrl = 'data:text/plain;base64,' + btoa(compiledCode);
+      return [brythonUrl, ...stdlib, loaderUrl, compiledCodeUrl];
     },
     scriptType: 'text/python',
   },
