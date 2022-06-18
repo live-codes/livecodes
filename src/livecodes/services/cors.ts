af095c4ea6974abadee85d4b73c30f4bf0f126bb
Hatem Hosny <hatemhosny@gmail.com>
2022-06-18 08:00:37 +0200

    use raw response

--- src/livecodes/services/cors.ts
+++ src/livecodes/services/cors.ts
@@ -2,25 +2,14 @@ import { fetchWithHandler } from '../utils';
 import { allowedOrigin, whitelistTarget } from './allowed-origin';
 
 const corsApiUrl = 'https://api.livecodes.io/cors?url=';
-const proxyUrl = 'https://api.allorigins.win/get?url=';
+const proxyUrl = 'https://api.allorigins.win/raw?url=';
 
 export const corsService = {
   fetch: async (url: string, options?: RequestInit) => {
-    if (allowedOrigin()) {
-      const corsUrl = corsApiUrl + encodeURIComponent(url);
-      if (whitelistTarget(url)) {
-        return fetchWithHandler(corsUrl, options);
-      }
-      return fetchWithHandler(url, options).catch(() => fetchWithHandler(corsUrl, options));
-    } else {
-      const corsUrl = proxyUrl + encodeURIComponent(url);
-      const getData = (res: Response) => res.json().then((data) => new Response(data.contents));
-      if (whitelistTarget(url)) {
-        return fetchWithHandler(corsUrl, options).then(getData);
-      }
-      return fetchWithHandler(url, options)
-        .catch(() => fetchWithHandler(corsUrl, options))
-        .then(getData);
+    const corsUrl = (!allowedOrigin() ? corsApiUrl : proxyUrl) + encodeURIComponent(url);
+    if (whitelistTarget(url)) {
+      return fetchWithHandler(corsUrl, options);
     }
+    return fetchWithHandler(url, options).catch(() => fetchWithHandler(corsUrl, options));
   },
 };
