b744588acdfa82f1d206d5aa950d657224657d66
Hatem Hosny <hatemhosny@gmail.com>
2022-06-18 07:42:01 +0200

    feat(services): add cors service for non-allowed origins

--- src/livecodes/services/cors.ts
+++ src/livecodes/services/cors.ts
@@ -1,19 +1,26 @@
 import { fetchWithHandler } from '../utils';
 import { allowedOrigin, whitelistTarget } from './allowed-origin';
 
-const corsApiUrl = 'https://api.livecodes.io/cors';
+const corsApiUrl = 'https://api.livecodes.io/cors?url=';
+const proxyUrl = 'https://api.allorigins.win/get?url=';
 
 export const corsService = {
   fetch: async (url: string, options?: RequestInit) => {
-    if (!allowedOrigin()) {
-      return fetchWithHandler(url, options);
+    if (allowedOrigin()) {
+      const corsUrl = corsApiUrl + encodeURIComponent(url);
+      if (whitelistTarget(url)) {
+        return fetchWithHandler(corsUrl, options);
+      }
+      return fetchWithHandler(url, options).catch(() => fetchWithHandler(corsUrl, options));
+    } else {
+      const corsUrl = proxyUrl + encodeURIComponent(url);
+      const getData = (res: Response) => res.json().then((data) => new Response(data.contents));
+      if (whitelistTarget(url)) {
+        return fetchWithHandler(corsUrl, options).then(getData);
+      }
+      return fetchWithHandler(url, options)
+        .catch(() => fetchWithHandler(corsUrl, options))
+        .then(getData);
     }
-
-    const corsUrl = corsApiUrl + '?url=' + encodeURIComponent(url);
-    if (whitelistTarget(url)) {
-      return fetchWithHandler(corsUrl, options);
-    }
-
-    return fetchWithHandler(url, options).catch(() => fetchWithHandler(corsUrl, options));
   },
 };
