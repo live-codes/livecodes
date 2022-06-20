0a23a2f5254c64572a4133a10cfc4e2a947bd915
Hatem Hosny <hatemhosny@gmail.com>
2022-06-21 01:27:07 +0200

    ci: bundlewatch config

--- .bundlewatch.config.js
+++ .bundlewatch.config.js
@@ -3,34 +3,29 @@ module.exports = {
     {
       path: './build/assets/*.js',
       maxSize: '10kB',
-      compression: 'brotli',
     },
     {
       path: './build/livecodes/!(lang-)*.js',
       maxSize: '30kB',
-      compression: 'brotli',
     },
     {
       path: './build/livecodes/*(app|embed|lite).*.js',
       maxSize: '100kB',
-      compression: 'brotli',
     },
     {
       path: './build/livecodes/codemirror-*.js',
       maxSize: '300kB',
-      compression: 'brotli',
     },
     // {
     //   path: './build/livecodes/lang-*.js',
     //   maxSize: '10kB',
-    //   compression: 'brotli',
     // },
     {
       path: './build/livecodes/*.css',
       maxSize: '10kB',
-      compression: 'brotli',
     },
   ],
+  defaultCompression: 'brotli',
   normalizeFilenames: /^.+?(\.[^.]+?)\.\w+$/,
   ci: {
     trackBranches: ['main', 'develop'],
