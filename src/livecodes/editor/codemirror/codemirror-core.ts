07e10fc640b04b5e90af26c053b0506b8476caac
Hatem Hosny <hatemhosny@gmail.com>
2022-10-26 00:13:24 +0200

    organize codemirror imports

--- src/livecodes/editor/codemirror/codemirror-core.ts
+++ src/livecodes/editor/codemirror/codemirror-core.ts
@@ -4,11 +4,21 @@ export * from '@codemirror/language';
 export * from '@codemirror/lint';
 export * from '@codemirror/search';
 export * from '@codemirror/state';
-export * from '@codemirror/theme-one-dark';
 export * from '@codemirror/view';
-export * from '@codemirror/lang-html';
+
+export {
+  TagSpec,
+  html,
+  htmlCompletionSource,
+  htmlCompletionSourceWith,
+  htmlLanguage,
+} from '@codemirror/lang-html';
 export * from '@codemirror/lang-css';
-export { javascriptLanguage, javascript } from '@codemirror/lang-javascript';
+export * from '@codemirror/lang-javascript';
+export * from '@codemirror/lang-json';
+
+export * from '@codemirror/theme-one-dark';
+
 export * from '@lezer/common';
 export * from '@lezer/highlight';
 export * from '@lezer/lr';
