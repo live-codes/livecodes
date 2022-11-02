616eb14cd2ef5d6299279e005f4bd5449fa17fdb
Hatem Hosny <hatemhosny@gmail.com>
2022-11-02 09:44:12 +0200

    add extra codemirror extensions

---
+++ src/livecodes/editor/codemirror/extras.ts
@@ -0,0 +1,3 @@
+export { indentationMarkers } from '@replit/codemirror-indentation-markers';
+export { vscodeKeymap } from '@replit/codemirror-vscode-keymap';
+export { colorPicker } from '@replit/codemirror-css-color-picker';
