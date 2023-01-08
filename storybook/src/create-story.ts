53388f9e2c5fa512c48a8dae3fc97971745ca88f
Hatem Hosny <hatemhosny@gmail.com>
2023-01-08 07:41:49 +0200

    fix

--- storybook/src/create-story.ts
+++ storybook/src/create-story.ts
@@ -277,7 +277,7 @@ const argTypes: Partial<ArgTypes<LiveCodesArgs>> = {
 };
 
 const appUrl =
-  location.hostname.startsWith('localhost') || location.hostname.startsWith('localhost')
+  location.hostname.startsWith('localhost') || location.hostname.startsWith('127.0.0.1')
     ? 'http://127.0.0.1:8080'
     : location.origin;
 
