9debd3798317ef138582af1d7cc3c86c204c2020
Hatem Hosny <hatemhosny@gmail.com>
2023-01-15 02:03:44 +0200

    remove redundant `sandbox` attribute from main app iframe

--- src/livecodes/main.ts
+++ src/livecodes/main.ts
@@ -61,10 +61,6 @@ export const livecodes = async (container: string, config: Partial<Config> = {})
       const iframe = document.createElement('iframe');
       iframe.name = 'app';
       iframe.style.display = 'none';
-      iframe.setAttribute(
-        'sandbox',
-        'allow-same-origin allow-downloads allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-presentation allow-scripts',
-      );
 
       containerElement.appendChild(iframe);
       iframe.contentWindow?.document.open();
