6bde609c917068e197bd9290af0bbe0161df5a85
Hatem Hosny <hatemhosny@gmail.com>
2023-04-27 21:32:46 +0200

    doc: reset custom content

--- docs/src/custom-content.ts
+++ docs/src/custom-content.ts
@@ -1,19 +1,11 @@
 import { createContext } from 'react';
 
-export const defaultDocContent = 'Default Doc Content';
-export const defaultTocContent = 'Default TOC Content';
+export const defaultDocContent = '';
+export const defaultTocContent = '';
 
-export const getNewDocContent = () => {
-  // return '';
-  const rnd = Math.floor(Math.random() * 100);
-  return 'New Doc content ' + rnd;
-};
+export const getNewDocContent = () => '';
 
-export const getNewTocContent = () => {
-  // return '';
-  const rnd = Math.floor(Math.random() * 100);
-  return 'New TOC content ' + rnd;
-};
+export const getNewTocContent = () => '';
 
 export const CustomContentContext = createContext({
   docContent: defaultDocContent,
