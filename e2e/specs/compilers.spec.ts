8359bf572b3c8cc8f05debcb9926f48482aee712
Hatem Hosny <hatemhosny@gmail.com>
2023-07-03 04:52:50 +0300

    add test for vue sfc languages

--- e2e/specs/compilers.spec.ts
+++ e2e/specs/compilers.spec.ts
@@ -1091,6 +1091,58 @@ h1 {
     expect(buttonText).toBe('Count is: 3, double is: 6');
   });
 
+  test('Vue 3 langs', async ({ page, getTestUrl }) => {
+    const sfc = `<template lang="md">
+  # Hello, {{ name }}!
+  <p :class="$style.text">some text</p>
+</template>
+
+<script lang="ts" setup>
+  const name: string = 'Vue 3';
+</script>
+
+<style lang="scss" module>
+  $font-stack: Helvetica, sans-serif;
+  $primary-color: #555;
+  $secondary-color: green;
+
+  h1 {
+    font: 200% $font-stack;
+    color: $primary-color;
+  }
+
+  .text {
+    font: 100% $font-stack;
+    color: $secondary-color;
+  }
+</style>
+`;
+
+    await page.goto(
+      getTestUrl({
+        x: 'code/N4IgLglmA2CmIC4QFUB2kawCYAIAKATgPYBWsAxmCADQhawDO5BEADpEaoiDeAIYBzBogDaAXVp9KEAG6wAolihEC3Ji3a8AtnwIBrAK6tEoaH1QCDg+EgAWYLdF7lOYWOm4gAvrQZgAnnAmIGYWVgI2IOQMwrQu6O5USN6+zGxJpuaW1twyBvBxroncADxuWqxmbjihAgC8ADogWlhNAHwNBA2oODgAxDgAErDQ0ETUOMDAOKh8WrA4Xl4AhJ3dvSWsOAjkZjGNIAAkfoGwAHRuAB5g7QxE8zhXYCUA9KwdXaiv5ZV8bh-dNZfdTpGpZA5gBhNHAMWBgIwAno4eJ+GZzWAIGFgFgWHB1HAAcgAavkcABmAkAbiBrxB7ERNJOcDBFgOTBi0K0RCwBjgiN6hwAZq4ALR+KR6THDaBySDkPgTBjmBhi2AsQXUz4C1gsHQEfwilxjAiYvoAVgtmvWOGOFE4WF0BqNKkxAgIsHcVtQQN6tgAjJMfb0cML0JiAEwABkjAFIbaGwGKwBKvcHkURjZjDjqIHqnRmVKnFkCgxdYNdA1rgwnMX7o3GhaLxeQ9EXes6TTbYfEHfrDQWCEWvDSXkzYG0UiAxwxbB7IaIJFO0uxhAhxHEYoRGHDPLQdURyIw7gRV+uogY-PcAMpwyAWVfAHwgXOsFTzhCP2gBViMExPtx+A+IRZOEkTfowy5UIUCQeMkT5yCeECcNwAAs3hAA',
+      }),
+    );
+
+    const { app, getResult, waitForResultUpdate } = await getLoadedApp(page);
+
+    await app.click(':nth-match([data-hint="Change Language"], 3)');
+    await app.click('text=Vue 3 SFC');
+    await waitForEditorFocus(app);
+    // await page.keyboard.insertText(sfc);
+
+    await waitForResultUpdate();
+
+    const headerText = await getResult().innerText('h1');
+
+    // markdown, scss, typescript
+    expect(headerText).toContain(`Hello, Vue 3!`);
+    expect(await getResult().$eval('h1', (e) => getComputedStyle(e).color)).toBe('rgb(85, 85, 85)');
+
+    // css modules
+    expect(await getResult().$eval('p', (e) => getComputedStyle(e).color)).toBe('rgb(0, 128, 0)');
+  });
+
   test('Vue 2', async ({ page, getTestUrl }) => {
     await page.goto(getTestUrl());
 
