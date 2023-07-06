9d5ca48ec70c32ec02dd340957f2c820f41df931
Hatem Hosny <hatemhosny@gmail.com>
2023-07-06 23:58:13 +0300

    feat(compilers): allow svelte SFC to import and compile other SFCs

--- src/livecodes/languages/svelte/lang-svelte-compiler.ts
+++ src/livecodes/languages/svelte/lang-svelte-compiler.ts
@@ -1,24 +1,49 @@
-import type { CompilerFunction } from '../../models';
-import { compileAllBlocks } from '../../compiler';
-import { getLanguageCustomSettings } from '../../utils';
+/* eslint-disable import/no-internal-modules */
+import type { CompilerFunction, Config } from '../../models';
+import { compileAllBlocks } from '../../compiler/compile-blocks';
+import { createImportMap, replaceSFCImports } from '../../compiler/import-map';
+import { getLanguageCustomSettings, getLanguageByAlias } from '../utils';
+import { getCompileResult } from '../../compiler';
 
-(self as any).createSvelteCompiler =
-  (): CompilerFunction =>
-  async (code, { config }) => {
-    const processedCode = await compileAllBlocks(code, config, {
+(self as any).createSvelteCompiler = (): CompilerFunction => {
+  let importedContent = '';
+
+  const compileSvelteSFC = async (
+    code: string,
+    { config, filename }: { config: Config; filename: string },
+  ) => {
+    if (!code) return getCompileResult('');
+
+    const fullCode = await replaceSFCImports(code, {
+      config,
+      filename,
+      getLanguageByAlias,
+      sfcExtension: '.svelte',
+      compileSFC: async (
+        code: string,
+        { config, filename }: { config: Config; filename: string },
+      ) => {
+        importedContent += `\n${filename}\n\n${code}\n`;
+        return (await compileSvelteSFC(code, { config, filename })).code;
+      },
+    });
+    const processedCode = await compileAllBlocks(fullCode, config, {
       removeEnclosingTemplate: true,
     });
     const customSettings = getLanguageCustomSettings('svelte', config);
-    const customElement = customSettings.customElement;
     const init =
-      customElement === true
+      filename === 'App.svelte'
         ? ''
-        : `\n
-new Component({ target: document.querySelector("#livecodes-app") || document.body.appendChild(document.createElement('div')) });
-`;
+        : `\nnew Component({ target: document.querySelector("#livecodes-app") || document.body.appendChild(document.createElement('div')) });`;
     const { js } = (window as any).svelte.compile(processedCode, {
       css: 'injected',
       ...customSettings,
     });
-    return js.code + init;
+    return {
+      code: js.code + init,
+      info: { importedContent, imports: createImportMap(importedContent, config) },
+    };
   };
+
+  return (code, { config }) => compileSvelteSFC(code, { config, filename: 'App.svelte' });
+};
