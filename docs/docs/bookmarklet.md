113ccd835db58a4f2cb06fd7cae8adbc9b8e1331
Hatem Hosny <hatemhosny@gmail.com>
2023-07-16 16:55:03 +0300

    edit docs

--- docs/docs/bookmarklet.md
+++ docs/docs/bookmarklet.md
@@ -8,9 +8,11 @@ Instead of manually copy/pasting URLs to import, adding **"Edit in LiveCodes"** 
 
 Drag this link to the browser bookmarks bar:
 
-<a href='javascript:(()=>{window.open("https://livecodes.io/?x="+encodeURIComponent(location.href),"_blank");})();'>Edit in LiveCodes</a>
+<div
+dangerouslySetInnerHTML={{__html: `<a href='javascript:(()=>{window.open("https://livecodes.io/?x="+encodeURIComponent(location.href),"_blank");})();'>Edit in LiveCodes</a>`}}
+/>
 
-<br /><br />
+<br />
 
 or manually create a new bookmark in your browser and add this code as its URL:
 
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -200,7 +200,7 @@ const config = {
                 href: 'https://www.npmjs.com/package/livecodes',
               },
               {
-                html: '<a href="https://status.livecodes.io" target="_blank" rel="noopener noreferrer" class="footer__link-item status-link">Status<svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></a>',
+                html: '<a href="https://status.livecodes.io" target="_blank" rel="noopener noreferrer" class="footer__link-item status-link"><span>Status</span><svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></a>',
               },
             ],
           },
