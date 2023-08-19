64d6714476ecaee6e4ab8978165247f0f62098e3
Adam <adam@greenough.me>
2023-08-19 13:40:35 +0100

    Linter fixes

--- src/livecodes/UI/selectors.ts
+++ src/livecodes/UI/selectors.ts
@@ -415,7 +415,7 @@ export const getEditorSettingsFormatLink = (editorSettingsContainer: HTMLElement
 export const getWelcomeLinkNew = (welcomeContainer: HTMLElement) =>
   welcomeContainer.querySelector('#welcome-link-new') as HTMLAnchorElement;
 
-  export const getWelcomeLinkNewHero = (welcomeContainer: HTMLElement) =>
+export const getWelcomeLinkNewHero = (welcomeContainer: HTMLElement) =>
   welcomeContainer.querySelector('#welcome-link-new-hero') as HTMLAnchorElement;
 
 export const getWelcomeLinkOpen = (welcomeContainer: HTMLElement) =>
