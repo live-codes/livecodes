07fafe4db275b63774afab3f245943c54f8a016e
Hatem Hosny <hatemhosny@gmail.com>
2023-09-23 00:19:55 +0300

    feat(compilers): clean up pyodide state between reloads

--- src/livecodes/languages/python-pyodide/lang-python-pyodide-script.ts
+++ src/livecodes/languages/python-pyodide/lang-python-pyodide-script.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { pyodideBaseUrl } from '../../vendors';
 
 declare const loadPyodide: any;
@@ -12,6 +13,21 @@ window.addEventListener('load', async () => {
   let code = '';
   const scripts = document.querySelectorAll('script[type="text/python"]');
   scripts.forEach((script) => (code += script.innerHTML + '\n'));
+
+  async function cleanUp() {
+    if (!livecodes.pyodideState) return;
+    try {
+      // https://pyodide-components.readthedocs.io/en/latest/faster_pyodide_testing.html#clean-up-worker-tests
+      // https://github.com/pyodide/pyodide/blob/main/src/py/pyodide/_state.py
+      // https://github.com/pyodide/pyodide/pull/1349
+      // https://github.com/pyodide/pyodide/issues/703
+      livecodes.pyodide.pyodide_py._state.restore_state(livecodes.pyodideState);
+    } catch (err) {
+      // if restoring state fails, reload pyodide
+      livecodes.pyodideLoading = true;
+      await main();
+    }
+  }
   async function main() {
     if (livecodes.pyodideLoading === false) return;
     livecodes.pyodide = await loadPyodide({
@@ -22,7 +38,9 @@ window.addEventListener('load', async () => {
   const pyodideReady = main();
   async function evaluatePython(code: string) {
     await pyodideReady;
+    await cleanUp();
     try {
+      livecodes.pyodideState = livecodes.pyodide.pyodide_py._state.save_state();
       await livecodes.pyodide.loadPackagesFromImports(code);
       await livecodes.pyodide.runPythonAsync(code);
     } catch (err) {
@@ -32,4 +50,6 @@ window.addEventListener('load', async () => {
   }
   await evaluatePython(code);
   parent.postMessage({ type: 'loading', payload: false }, '*');
+
+  // clean up
 });
