707aa545810d30be7e639c971d6cfc681a9edef7
Hatem Hosny <hatemhosny@gmail.com>
2023-09-30 10:19:30 +0300

    feat(compilers): expose R output and plots as `livecodes.r.output` and `livecodes.r.plots`

--- src/livecodes/languages/r/lang-r-script-esm.ts
+++ src/livecodes/languages/r/lang-r-script-esm.ts
@@ -14,21 +14,34 @@ interface RunOptions {
   container?: Element | string | null;
   canvasHeight?: number;
   canvasWidth?: number;
+  canvasPointSize?: number;
+  canvasBackground?: string;
   env?: any;
 }
 
 const defaultConfig = {
   canvasHeight: 309,
   canvasWidth: 500,
+  canvasPointSize: 12,
+  canvasBackground: 'transparent',
 };
 
 livecodes.r.packages = [];
 livecodes.r.run =
   livecodes.r.run ||
-  (async ({ code, container, canvasHeight, canvasWidth, env }: RunOptions = {}) => {
+  (async ({
+    code,
+    container,
+    canvasHeight,
+    canvasWidth,
+    canvasPointSize,
+    canvasBackground,
+    env,
+  }: RunOptions = {}) => {
     parent.postMessage({ type: 'loading', payload: true }, '*');
 
     livecodes.r.output = '';
+    livecodes.r.plots = [];
 
     if (container !== null && livecodes.r.config?.container !== null) {
       if (typeof container === 'string') {
@@ -45,6 +58,14 @@ livecodes.r.run =
       defaultConfig.canvasHeight;
     canvasWidth =
       Number(canvasWidth) || Number(livecodes.r.config?.canvasWidth) || defaultConfig.canvasWidth;
+    canvasPointSize =
+      Number(canvasPointSize) ||
+      Number(livecodes.r.config?.canvasPointSize) ||
+      defaultConfig.canvasPointSize;
+    canvasBackground =
+      String(canvasBackground) ||
+      String(livecodes.r.config?.canvasBackground) ||
+      defaultConfig.canvasBackground;
 
     if (code == null) {
       code = '';
@@ -81,39 +102,38 @@ livecodes.r.run =
       try {
         await webR.evalRVoid('dev.off()');
 
-        const getOutput = (type: 'stdout' | 'stderr') =>
+        const getOutput = (type: 'stdout' | 'stderr' | 'all') =>
           result?.output
-            .filter((evt) => evt.type === type)
+            .filter((evt) => type === 'all' || evt.type === type)
             .map((evt) => evt.data)
             .join('\n') || '';
 
+        const output = getOutput('all');
         const stdout = getOutput('stdout');
         const stderr = getOutput('stderr');
         if (stderr.trim()) {
           console.log(stderr);
         }
 
-        livecodes.r.output = stdout;
-
         const msgs = await webR.flush();
 
-        if (container && typeof container !== 'string') {
-          msgs.forEach((msg: { type: 'canvas'; data: { event: string; image: ImageBitmap } }) => {
-            if (msg.type === 'canvas' && msg.data.event === 'canvasNewPage') {
-              canvas = document.createElement('canvas');
-              canvas.setAttribute('width', String(2 * canvasWidth!));
-              canvas.setAttribute('height', String(2 * canvasHeight!));
-              canvas.style.width = `${canvasWidth}px`;
-              canvas.style.display = 'block';
-              canvas.style.margin = 'auto';
-              canvasList.push(canvas);
-            }
-            if (msg.type === 'canvas' && msg.data.event === 'canvasImage' && canvas) {
-              const ctx = canvas.getContext('2d');
-              ctx?.drawImage(msg.data.image, 0, 0);
-            }
-          });
+        msgs.forEach((msg: { type: 'canvas'; data: { event: string; image: ImageBitmap } }) => {
+          if (msg.type === 'canvas' && msg.data.event === 'canvasNewPage') {
+            canvas = document.createElement('canvas');
+            canvas.setAttribute('width', String(2 * canvasWidth!));
+            canvas.setAttribute('height', String(2 * canvasHeight!));
+            canvas.style.width = `${canvasWidth}px`;
+            canvas.style.display = 'block';
+            canvas.style.margin = 'auto';
+            canvasList.push(canvas);
+          }
+          if (msg.type === 'canvas' && msg.data.event === 'canvasImage' && canvas) {
+            const ctx = canvas.getContext('2d');
+            ctx?.drawImage(msg.data.image, 0, 0);
+          }
+        });
 
+        if (container && typeof container !== 'string') {
           container.innerHTML = '';
           const pre = document.createElement('pre');
           if (/\S/.test(stdout)) {
@@ -131,6 +151,9 @@ livecodes.r.run =
             container.appendChild(div);
           }
         }
+
+        livecodes.r.output = output;
+        livecodes.r.plots = canvasList;
       } finally {
         webRCodeShelter.purge();
       }
