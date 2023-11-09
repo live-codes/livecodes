8f3cdf0a94560037c3eacba48214e2b08cd64ef7
Hatem Hosny <hatemhosny@gmail.com>
2023-11-10 01:42:21 +0200

    add tests

--- src/livecodes/services/sandbox.ts
+++ src/livecodes/services/sandbox.ts
@@ -2,7 +2,7 @@ const cfPagesBaseUrl = 'https://livecodes-sandbox.pages.dev';
 const ghPagesBaseUrl = 'https://live-codes.github.io/livecodes-sandbox/dist';
 
 const serviceBaseUrl = process.env.CI ? ghPagesBaseUrl : cfPagesBaseUrl;
-const version = 'v7';
+const version = 'v8';
 
 export const sandboxService = {
   getResultUrl: () => `${serviceBaseUrl}/${version}/`,
