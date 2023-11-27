cec1e3571c2448e10df960762fd961ea595ac052
Hatem Hosny <hatemhosny@gmail.com>
2023-11-27 20:43:04 +0200

    upgrade chai

--- src/livecodes/vendors.ts
+++ src/livecodes/vendors.ts
@@ -40,9 +40,9 @@ export const browserJestUrl = /* @__PURE__ */ getUrl(
 
 export const brythonBaseUrl = /* @__PURE__ */ getUrl('brython@3.11.3/');
 
-export const chaiUrl = /* @__PURE__ */ getModuleUrl('chai@4.3.6');
+export const chaiUrl = /* @__PURE__ */ getModuleUrl('chai@5.0.0-alpha.2');
 
-export const chaiTypesUrl = /* @__PURE__ */ getUrl('@types/chai@4.2.22/index.d.ts');
+export const chaiTypesUrl = /* @__PURE__ */ getUrl('@types/chai@4.3.11/index.d.ts');
 
 export const cherryCljsBaseUrl = /* @__PURE__ */ getUrl('cherry-cljs@0.0.4/');
 
