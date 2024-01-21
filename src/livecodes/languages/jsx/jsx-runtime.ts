40a96c7148d5a41e029442e408142aa9b3d7772b
Hatem Hosny <hatemhosny@gmail.com>
2024-01-22 01:14:04 +0200

    only render jsx default export if it is a react component

--- src/livecodes/languages/jsx/jsx-runtime.ts
+++ src/livecodes/languages/jsx/jsx-runtime.ts
@@ -1,9 +1,13 @@
 export const reactRuntime = `
 import { jsx as _jsx } from "react/jsx-runtime";
 import { createRoot } from "react-dom/client";
-import App from './script';
-const root = createRoot(document.querySelector("#livecodes-app") || document.body.appendChild(document.createElement('div')));
-root.render(_jsx(App, {}));
+import App from "./script";
+(() => {
+  const isReactComponent = (c) => typeof c === "function" && (/return\\s+\\(?\\s*(_jsx|React\\.createElement)/g.test(String(c)) || Boolean(c.prototype.isReactComponent));
+  if (!isReactComponent(App)) return;
+  const root = createRoot(document.querySelector("#livecodes-app") || document.body.appendChild(document.createElement("div")));
+  root.render(_jsx(App, {}));
+})();
 `;
 
 export const hasCustomJsxRuntime = (code: string) => new RegExp(/\/\*\*[\s\*]*@jsx\s/g).test(code);
