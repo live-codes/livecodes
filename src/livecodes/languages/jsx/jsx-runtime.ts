c853df67a07e5954f5e8f138f385ef02d0e82e95
Hatem Hosny <hatemhosny@gmail.com>
2024-01-23 03:20:17 +0200

    fix jsx runtime code

--- src/livecodes/languages/jsx/jsx-runtime.ts
+++ src/livecodes/languages/jsx/jsx-runtime.ts
@@ -3,7 +3,7 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { createRoot } from "react-dom/client";
 import App from "./script";
 (() => {
-  const isReactComponent = (c) => typeof c === "function" && (/return\\s+\\(?\\s*(_jsx|React\\.createElement)/g.test(String(c)) || Boolean(c.prototype.isReactComponent));
+  const isReactComponent = (c) => typeof c === "function" && (/(return|=>)\\s+\\(?\\s*(_jsx|React\\.createElement)/g.test(String(c)) || Boolean(c.prototype?.isReactComponent));
   if (!isReactComponent(App)) return;
   const root = createRoot(document.querySelector("#livecodes-app") || document.body.appendChild(document.createElement("div")));
   root.render(_jsx(App, {}));
