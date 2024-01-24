aabfdee22aa72dac42b864cd12854a8ce64b2765
Hatem Hosny <hatemhosny@gmail.com>
2024-01-24 12:04:17 +0200

    improve detecting react jsx

--- src/livecodes/languages/react-native/react-native-runtime.ts
+++ src/livecodes/languages/react-native/react-native-runtime.ts
@@ -1,11 +1,13 @@
+// eslint-disable-next-line import/no-internal-modules
+import { checkIsReact } from '../jsx/react-runtime';
+
 export const reactNativeRuntime = `
 import { AppRegistry } from "react-native";
 import App from "./script";
 (() => {
-  const isReactComponent = (c) => typeof c === "function" && (/(return|=>)\\s+\\(?\\s*(_jsx|React\\.createElement)/g.test(String(c)) || Boolean(c.prototype?.isReactComponent));
-  if (!isReactComponent(App)) return;
-  const rootTag = document.querySelector("#livecodes-app") || document.body.appendChild(document.createElement("div"));
-  AppRegistry.registerComponent("App", () => App);
-  AppRegistry.runApplication("App", { rootTag });
+${checkIsReact('App')}
+const rootTag = document.querySelector("#livecodes-app") || document.body.appendChild(document.createElement("div"));
+AppRegistry.registerComponent("App", () => App);
+AppRegistry.runApplication("App", { rootTag });
 })();
 `;
