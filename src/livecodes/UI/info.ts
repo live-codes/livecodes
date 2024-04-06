9149b3cb734a750f8e6a3d3ebf2cd9bc9d3f2f90
Hatem Hosny <hatemhosny@gmail.com>
2024-04-06 13:27:49 +0200

    fix

--- src/livecodes/UI/info.ts
+++ src/livecodes/UI/info.ts
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-internal-modules */
-import type { createEventsManager } from '../events';
 import { infoScreen } from '../html';
 import type { createModal } from '../modal';
 import * as UI from '../UI';
