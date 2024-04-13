7c942e3357131e2710a9bda93abab166083e1c51
zyf722 <1124982772@qq.com>
2024-04-13 22:40:56 +0800

    feat: abstract tag for innerHTML

--- src/livecodes/i18n/i18n.ts
+++ src/livecodes/i18n/i18n.ts
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-named-as-default-member */
 import i18n from 'i18next';
 import backend from 'i18next-http-backend';
+import { abstractifyHTML } from './utils';
 
 export const init = (lng: string | undefined, baseUrl: string) => {
   i18n.use(backend).init({
     lng,
-    debug: true,
+    debug: true,  // Remove this line in production
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
@@ -13,6 +14,19 @@ export const init = (lng: string | undefined, baseUrl: string) => {
     backend: {
       loadPath: baseUrl + 'translation-{{lng}}-{{ns}}.json',
     },
+
+    // To log innerHTML translations in console
+    // Remove following lines in production
+    saveMissing: true,
+    missingKeyHandler: (lngs, namespace, key, fallbackValue, updateMissing, options) => {
+      if (options.PROP === 'innerHTML') {
+        const { html, elements } = abstractifyHTML(fallbackValue);
+
+        // eslint-disable-next-line no-console
+        console.info(`Translation template for key: ${key}\n\nHTML:\n${html}\n\nElements:\n`, elements);
+      }
+    },
+    missingKeyNoValueFallbackToKey: true,
   });
 };
 
--- src/livecodes/i18n/utils.ts
+++ src/livecodes/i18n/utils.ts
@@ -1,6 +1,89 @@
 // eslint-disable-next-line import/no-internal-modules
 import { predefinedValues } from '../utils/utils';
 
+interface TagElement {
+  name: string;
+  attributes?: Record<string, string>;
+}
+
+// Only used in ./i18n.ts, could remove `export` once it's no longer used
+export const abstractifyHTML = (html: string) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  const elements: TagElement[] = [];
+  let counter = 0;
+
+  const replaceElement = (node: HTMLElement) => {
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      node.childNodes.forEach((child) => {
+        replaceElement(child as HTMLElement);
+      });
+
+      const name = node.tagName.toLowerCase();
+      if (name !== 'body') {
+        const attributes =
+          (node.attributes.length > 0) ?
+            Array.from(node.attributes).reduce((acc, attr) => {
+              acc[attr.name] = attr.value;
+              return acc;
+            }, {} as Record<string, string>) : undefined;
+        elements.push({ name, attributes });
+
+        // Create a new element with the counter as the tag name
+        const newElement = doc.createElement(`tag-${counter}`);
+        // Copy the children from the original node to the new element
+        while (node.firstChild) {
+          newElement.appendChild(node.firstChild);
+        }
+        // Replace the original node with the new element
+        if (node.parentNode) {
+          node.parentNode.replaceChild(newElement, node);
+        }
+        counter++;
+      }
+    }
+  };
+
+  replaceElement(doc.body);
+  return {
+    html: doc.body.innerHTML.replace(/tag-/g, '').replace(/\s+/g, ' ').trim(),
+    elements
+  };
+}
+
+const unabstractifyHTML = (html: string, elements: TagElement[]) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html.replace(/<(\/?)([0-9]+)([^>]*)>/g, '<$1tag-$2 $3>'), 'text/html');
+
+  elements.forEach((element, index) => {
+    const oldElement = doc.body.querySelector(`tag-${index}`)!;
+    const newElement = doc.createElement(element.name);
+
+    // Copy previously saved attributes to newElement
+    if (element.attributes) {
+      Object.entries(element.attributes).forEach(([key, value]) => {
+        newElement.setAttribute(key, value);
+      });
+    }
+
+    // Override attributes base on those from abstract tags (oldElement)
+    if (oldElement.attributes) {
+      Array.from(oldElement.attributes).forEach((attr) => {
+        newElement.setAttribute(attr.name, attr.value);
+      });
+    }
+
+    // Copy the children from oldElement to newElement
+    while (oldElement.firstChild) {
+      newElement.appendChild(oldElement.firstChild);
+    }
+
+    oldElement.replaceWith(newElement);
+  });
+
+  return doc.body.innerHTML;
+}
+
 export const translate = (
   container: HTMLElement,
   i18n: typeof import('./i18n').default | undefined,
@@ -21,7 +104,12 @@ export const translate = (
         el.dataset[prop] = i18n.t(lookupKey, el.dataset[prop]!, interpolation);
       } else {
         const translation = i18n.t(lookupKey, (el as any)[prop], interpolation);
-        (el as any)[prop] = translation;
+        if (prop === 'innerHTML' && el.innerHTML !== translation) {
+          const { elements } = abstractifyHTML(el.innerHTML);
+          el.innerHTML = unabstractifyHTML(translation, elements);
+        } else {
+          (el as any)[prop] = translation;
+        }
       }
     };
 
