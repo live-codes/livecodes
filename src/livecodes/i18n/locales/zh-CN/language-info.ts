3ef4c47c5720f81f61e5a19e8461f8e6f952a2dc
zyf722 <1124982772@qq.com>
2024-04-13 22:41:47 +0800

    chore: separate LangInfo namespace as different type

--- src/livecodes/i18n/locales/zh-CN/language-info.ts
+++ src/livecodes/i18n/locales/zh-CN/language-info.ts
@@ -1,6 +1,6 @@
-import type { I18nTranslation } from '../models';
+import type { I18nLangInfoTranslation } from '../models';
 
-const languageInfo: I18nTranslation = {
+const languageInfo: I18nLangInfoTranslation = {
   artTemplateDesc: {
     textContent: '高性能 JavaScript 模板引擎。',
   },
