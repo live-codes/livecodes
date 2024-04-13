a7de25a4b41c9cb921cd0e4bc95589b63c9e02f6
zyf722 <1124982772@qq.com>
2024-04-13 22:42:29 +0800

    i18n: add debug translations for abstract tags and predefined values

--- src/livecodes/i18n/locales/zh-CN/translation.ts
+++ src/livecodes/i18n/locales/zh-CN/translation.ts
@@ -1,7 +1,9 @@
 import type { I18nTranslation } from '../models';
 
 const translation: I18nTranslation = {
+  appVersion: 'App 版本：{{APP_VERSION}}',
   welcome: '欢迎',
+  livecodesAboutPara1: '<1><0>LiveCodes</0></1> 是一个<2>开源的</2>、<3>功能丰富的</3>、<4>客户端侧的</4>代码运行环境。目前支持<6>超过 80 种语言 / <5></5>框架</6>。它可以作为一个独立应用使用，亦可<7>嵌入</7>到任意网页中。可以通过多种途径将代码<8>预填</8>到编辑器中。',
 };
 
 export default translation;
