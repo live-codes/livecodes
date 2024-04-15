5e5d60de532087a0f276a872d74f68ffef5a6ab6
zyf722 <1124982772@qq.com>
2024-04-16 00:06:46 +0800

    i18n: add debug translation

---
+++ src/livecodes/i18n/locales/tmp/language-info.ts
@@ -0,0 +1,7 @@
+import { type I18nTranslationTemplate } from '../models';
+
+const languageInfo = {
+  artTemplateDesc: 'High performance JavaScript templating engine.',
+} as const satisfies I18nTranslationTemplate;
+
+export default languageInfo;
--- src/livecodes/i18n/locales/tmp/translation.ts
+++ src/livecodes/i18n/locales/tmp/translation.ts
@@ -1,12 +1,5 @@
 import { type I18nTranslationTemplate } from '../models';
 
-// This is used as a template for other translations.
-// Other translations should be typed like this:
-// const translation: I18nTranslation = { /* translation here */ };
-
-// Since we allow nested objects, it is important to distinguish I18nTranslationTemplate from I18nAttributes.
-// In view of this, properties declared in I18nAttributes (and those attributes might be used in future) shall not be used as a nested key.
-
 const translation = {
   livecodesAboutPara1:
     '<1><0>LiveCodes</0></1> is an <2>open-source</2>, <3>feature-rich</3>, <4>client-side</4> code playground. Currently, <6>80+ languages/<5></5>frameworks</6> are supported. It can be used as a standalone app or can be <7>embedded</7> in any web page. There are many ways to <8>prefill playgrounds</8> with code.',
