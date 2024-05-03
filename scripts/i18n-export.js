1f1f5071135cc5f0a5875ca99b99ccc182369975
zyf722 <1124982772@qq.com>
2024-05-04 00:15:59 +0800

    feat: extract i18n from .ts files with translateString

--- scripts/i18n-export.js
+++ scripts/i18n-export.js
@@ -1,37 +1,88 @@
 const fs = require('fs');
 const path = require('path');
 const jsdom = require('jsdom');
+const prettier = require('prettier');
+const babel = require('@babel/core');
+const parser = require('@babel/parser');
 
 const outDir = path.resolve('src/livecodes/i18n/locales/tmp');
-const baseDir = path.resolve('src/livecodes/html');
+const srcBaseDir = path.resolve('src/livecodes');
 
-/**
- * Generate .ts and .lokalise.json files from HTML files.
- * 
- * If no files are provided, it will process all HTML files in the base directory.
- */
-const generateTranslation = async () => {
-  const writeTranslation = (namespace) => {
-    // Use JSON.stringify to format the JSON object
-    // However we need to convert it to a valid TypeScript object
-    // so we need to remove the quotes from the keys and use single quotes in string
-    const name = namespace === 'translation' ? 'translation' : 'languageInfo';
-    const definition = JSON.stringify(trans[namespace], null, 2)
-      .replace(/"([^"]+)":/g, '$1:')
-      // .replace(/"(.*)\/\* COMMENTS \*\/(.*)"(,?)/g, "'$1'$3 // $2")
-      .replace(/"/g, "'");
-    const translationContent =
-      `import { type I18nTranslationTemplate } from '../models';\n\nconst ${name} = ` +
-      definition +
-      ` as const satisfies I18nTranslationTemplate;\n\nexport default ${name};`;
-    fs.writeFileSync(path.resolve(outDir, namespace + '.ts'), translationContent);
+const prettierConfig = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
+
+const trans = {
+  translation: {},
+  'language-info': {},
+};
+const structuredJSON = {
+  translation: {},
+  'language-info': {},
+};
+
+const writeTranslation = async (namespace) => {
+  const name = namespace === 'translation' ? 'translation' : 'languageInfo';
+  const code =
+    `import { type I18nTranslationTemplate } from '../models';\n\nconst ${name} = ` +
+    JSON.stringify(trans[namespace], null, 2) +
+    ` as const satisfies I18nTranslationTemplate;\n\nexport default ${name};`;
+
+  const translationContent = await prettier.format(code, {
+    parser: 'typescript',
+    ...prettierConfig,
+  });
+
+  await Promise.all([
+    fs.promises.writeFile(path.resolve(outDir, namespace + '.ts'), translationContent),
 
     // Save structured JSON for lokalise
-    fs.writeFileSync(path.resolve(outDir, namespace + '.lokalise.json'), JSON.stringify(structuredJSON[namespace], null, 2));
+    fs.promises.writeFile(
+      path.resolve(outDir, namespace + '.lokalise.json'),
+      JSON.stringify(structuredJSON[namespace], null, 2).replace(/<(\/?)([0-9]+)>/g, '<$1tag-$2>'),
+    ),
+  ]);
 
-    console.log(`Generated namespace ${namespace} in ${outDir}.`);
-  };
+  console.log(`Generated namespace ${namespace} in ${outDir}.`);
+};
 
+const addTranslation = (nsKey, value, desc, props) => {
+  nsKey = nsKey.split(':');
+  const key = nsKey.pop();
+  const namespace = nsKey.length === 1 ? nsKey.pop() : 'translation';
+
+  const parts = key.split('.');
+  let current = trans[namespace];
+  parts.forEach((part, index) => {
+    if (!current[part]) {
+      current[part] = index === parts.length - 1 ? value : {};
+    } else {
+      if (index === parts.length - 1 && current[part] !== value) {
+        console.error(`Duplicate key: ${key}`);
+      }
+    }
+    current = current[part];
+  });
+
+  if (!props || props.length == 1) {
+    structuredJSON[namespace][key] = {
+      translation: value,
+      notes: desc,
+    };
+  } else {
+    props.forEach((prop) => {
+      structuredJSON[namespace][key + `#${prop}`] = {
+        translation: value[prop],
+        notes: desc[prop],
+      };
+    });
+  }
+};
+
+const processHTML = async (files) => {
   // From src/livecodes/i18n/utils.ts
   const abstractifyHTML = (html) => {
     const doc = new jsdom.JSDOM(html).window.document;
@@ -77,125 +128,136 @@ const generateTranslation = async () => {
     };
   };
 
-  const files = process.argv.slice(2);
-  const trans = {
-    translation: {},
-    "language-info": {},
-  };
-  const structuredJSON = {
-    translation: {},
-    "language-info": {},
-  }
-
   const getValueAndContext = (element, prop) => {
     if (prop === 'innerHTML') {
       const { html, elements } = abstractifyHTML(element.innerHTML);
-      // return (
-      //   html +
-      //   '/* COMMENTS */' +
-      //   elements.map(
-      //     (el) =>
-      //       `<${el.name} ${
-      //         el.attributes
-      //           ? Object.keys(el.attributes)
-      //               .map((attr) => `${attr}="${el.attributes[attr]}"`)
-      //               .join(' ')
-      //           : ''
-      //       } />`,
-      //   )
-      // );
       return {
         value: html,
-        desc: elements.map(
-          (el, index) =>
-            `### <${index}> ###\n<${el.name} ${
-              el.attributes
-                ? Object.keys(el.attributes)
-                    .map((attr) => `${attr}="${el.attributes[attr]}"`)
-                    .join(' ')
-                : ''
-            } />\n\n`,
-        ).join(''),
+        desc: elements
+          .map(
+            (el, index) =>
+              `### <${index}> ###\n<${el.name} ${
+                el.attributes
+                  ? Object.keys(el.attributes)
+                      .map((attr) => `${attr}="${el.attributes[attr]}"`)
+                      .join(' ')
+                  : ''
+              } />\n\n`,
+          )
+          .join(''),
       };
     }
     return {
       value: element[prop].trim(),
-      desc: ''
+      desc: '',
     };
   };
-
-  if (files.some((file) => !file.endsWith('.html'))) {
-    console.error('Only HTML files are allowed.');
-    process.exit(1);
-  }
-
-  if (!files.length) {
-    files.push(...fs.readdirSync(baseDir).filter((file) => file.endsWith('.html')));
-  }
-
-  if (!fs.existsSync(outDir)) {
-    fs.mkdirSync(outDir);
-  }
 
   await Promise.all(
     files.map(async (file) => {
       try {
         console.log(`Processing ${file}...`);
 
-        const data = await fs.promises.readFile(path.resolve(baseDir, file), 'utf8');
+        const data = await fs.promises.readFile(file, 'utf8');
         const html = new jsdom.JSDOM(data).window.document;
 
         html.querySelectorAll('[data-i18n]').forEach((element) => {
-          const key = element.getAttribute('data-i18n').split(':').pop(); // This is for namespace
+          const key = element.getAttribute('data-i18n');
           const props = (element.getAttribute('data-i18n-prop') ?? 'textContent').split(' ');
-          const namespace = file === 'language-info.html' ? 'language-info' : 'translation';
 
-          const {value, desc} =
+          const { value, desc } =
             props.length == 1
               ? getValueAndContext(element, props[0])
-              : props.reduce((acc, prop) => {
-                  const vd = getValueAndContext(element, prop);
-                  acc.value[prop] = vd.value;
-                  acc.desc[prop] = vd.desc;
-                  return acc;
-                }, {value: {}, desc: {}});
+              : props.reduce(
+                  (acc, prop) => {
+                    const vd = getValueAndContext(element, prop);
+                    acc.value[prop] = vd.value;
+                    acc.desc[prop] = vd.desc;
+                    return acc;
+                  },
+                  { value: {}, desc: {} },
+                );
 
-          const parts = key.split('.');
-          let current = trans[namespace];
-          parts.forEach((part, index) => {
-            if (!current[part]) {
-              current[part] = index === parts.length - 1 ? value : {};
-            } else {
-              if (index === parts.length - 1) {
-                console.error(`Duplicate key: ${key}`);
-              }
-            }
-            current = current[part];
-          });
-
-          if (props.length == 1) {
-            structuredJSON[namespace][key] = {
-              translation: value,
-              notes: desc,
-            };
-          } else {
-            props.forEach((prop) => {
-              structuredJSON[namespace][key + `#${prop}`] = {
-                translation: value[prop],
-                notes: desc[prop],
-              };
-            });
-          }
+          addTranslation(key, value, desc, props);
         });
       } catch (err) {
         console.error(err);
       }
     }),
   );
+};
+
+const processTS = async (files) => {
+  await Promise.all(
+    files.map(async (file) => {
+      try {
+        console.log(`Processing ${file}...`);
+
+        const data = await fs.promises.readFile(file, 'utf8');
+        const ast = parser.parse(data, {
+          sourceType: 'module',
+          plugins: ['typescript'],
+        });
+
+        // Find all following two calls
+        babel.traverse(ast, {
+          CallExpression(path) {
+            if (
+              path.node.callee.type === 'MemberExpression' &&
+              path.node.callee.property.type === 'Identifier' &&
+              path.node.callee.property.name === 'translateString' &&
+              path.node.arguments.length >= 2 &&
+              path.node.arguments[0].type === 'StringLiteral' &&
+              path.node.arguments[1].type === 'StringLiteral'
+            ) {
+              addTranslation(
+                path.node.arguments[0].value,
+                path.node.arguments[1].value,
+                '',
+                undefined,
+              );
+            }
+          },
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    }),
+  );
+};
+
+/**
+ * Generate .ts and .lokalise.json files from HTML files.
+ *
+ * If no files are provided, it will process all relevant files in the base directory.
+ */
+const generateTranslation = async () => {
+  const files = process.argv.slice(2);
+  const HTMLFiles = [],
+    TSFiles = [];
+
+  if (!files.length) {
+    files.push(...fs.readdirSync(srcBaseDir, { recursive: true }));
+  }
+
+  HTMLFiles.push(
+    ...files
+      .filter((file) => file.endsWith('.html') && file.startsWith('html\\'))
+      .map((file) => path.resolve(srcBaseDir, file)),
+  );
+  TSFiles.push(
+    ...files.filter((file) => file.endsWith('.ts')).map((file) => path.resolve(srcBaseDir, file)),
+  );
+
+  if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir);
+  }
+
+  await processHTML(HTMLFiles);
+  await processTS(TSFiles);
 
   writeTranslation('translation');
-
-  if (Object.keys(trans["language-info"]).length > 0) {
+  if (Object.keys(trans['language-info']).length > 0) {
     writeTranslation('language-info');
   }
 };
