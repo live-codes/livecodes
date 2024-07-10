2c4b49a2012706f84492088ba443beef438c8863
zyf722 <1124982772@qq.com>
2024-07-10 22:20:12 +0800

    feat(i18n): deprecate outdated keys when importing

--- scripts/i18n-import.mjs
+++ scripts/i18n-import.mjs
@@ -7,18 +7,23 @@ import prettier from 'prettier';
 import { exit } from 'process';
 
 const outDir = path.resolve('src/livecodes/i18n/locales');
+const tmpDir = path.join(outDir, 'tmp');
 const api = new LokaliseApi({ apiKey: process.env.LOKALISE_API_TOKEN });
 const projectID = process.env.LOKALISE_PROJECT_ID;
 
 /**
  * Expand the flattened translation object (KV pairs) to a nested object.
  * @param {string} source Path to the source file.
+ * @param {Set<string>} validKeys Set of valid keys.
  * @returns object
  */
-const generateTranslationObject = (source) => {
-  const translations = JSON.parse(fs.readFileSync(source, 'utf-8'));
+const generateTranslationObject = async (source, validKeys) => {
+  const translations = JSON.parse(await fs.promises.readFile(source, 'utf-8'));
   const translationObject = {};
   for (const key in translations) {
+    if (!validKeys.has(key)) {
+      continue;
+    }
     const keys = key.split('.');
     const lastKey = keys.pop();
     let currentObject = translationObject;
@@ -52,80 +57,113 @@ const importFromLokalise = async () => {
   console.log('Fetching translations from Lokalise...');
 
   // Make a tmp directory to store the downloaded files
-  const tmpDir = path.resolve(process.env.LOKALISE_TEMP);
+  const lokaliseTempDir = path.resolve(process.env.LOKALISE_TEMP);
 
   const response = await api.files().download(`${projectID}:${branchName}`, {
     format: 'json',
     original_filenames: true,
+    placeholder_format: "i18n",
   });
   console.log(`Downloading zip file from ${response.bundle_url}`);
 
-  const zipPath = path.join(tmpDir, 'locales.zip');
+  const zipPath = path.join(lokaliseTempDir, 'locales.zip');
   const zipFile = await fetch(response.bundle_url);
-  fs.writeFileSync(zipPath, Buffer.from(await zipFile.arrayBuffer()));
+  await fs.promises.writeFile(zipPath, Buffer.from(await zipFile.arrayBuffer()));
 
-  console.log(`Extracting zip file to ${tmpDir}...`);
-  execSync(`unzip -o ${zipPath} -d ${tmpDir}`);
-  fs.unlinkSync(zipPath);
+  console.log(`Extracting zip file to ${lokaliseTempDir}...`);
+  execSync(`unzip -o ${zipPath} -d ${lokaliseTempDir}`);
+  await fs.promises.unlink(zipPath);
 
-  fs.readdir(tmpDir, (err, languages) => {
-    if (err) {
-      console.error(err);
-      exit(1);
+  const languages = await fs.promises.readdir(lokaliseTempDir);
+  console.log(
+    `Extracted languages to tmp directory, ${languages.length} languages (including English) found.`,
+  );
+
+  console.log("Checking if translation keys are outdated...");
+  const localTranslation = {};
+
+  /** @type {{[s: string]: Set<string>}} */
+  const lokaliseTranslation = {};
+
+  execSync('npm run i18n-export -- --save-temp', { stdio: 'pipe' });
+  const localNamespaces = (await fs.promises.readdir(tmpDir)).filter((file) => file.endsWith('.lokalise.json'));
+  for (const file of localNamespaces) {
+    const namespace = file.split('.')[0];
+    const filePath = path.join(tmpDir, file);
+
+    /** @type {{[s: string]: {notes: string, translation: string}}} */
+    const fileContent = JSON.parse(await fs.promises.readFile(filePath, 'utf-8'));
+
+    localTranslation[namespace] = {};
+    for (const key in fileContent) {
+      localTranslation[namespace][key] = fileContent[key].translation;
+    }
+  }
+
+  const enPath = path.join(lokaliseTempDir, 'en');
+  const enFiles = await fs.promises.readdir(enPath);
+  for (const file of enFiles) {
+    const namespace = file.split('.')[0];
+    const filePath = path.join(enPath, file);
+
+    /** @type {{[s: string]: string}} */
+    const fileContent = JSON.parse(await fs.promises.readFile(filePath, 'utf-8'));
+
+    lokaliseTranslation[namespace] = new Set();
+    for (const key in fileContent) {
+      if (!localTranslation[namespace][key]) {
+        console.warn(`Skipping: Key ${key} in namespace ${namespace} is missing in local translation.`);
+        continue;
+      } else if (localTranslation[namespace][key] !== fileContent[key]) {
+        console.warn(`Skipping: Key ${key} in namespace ${namespace} is outdated.`);
+        continue;
+      }
+      lokaliseTranslation[namespace].add(key);
+    }
+  };
+
+  for (let language of languages) {
+    const languagePath = path.join(lokaliseTempDir, language);
+    if (!(await fs.promises.stat(languagePath)).isDirectory() || language === 'en') {
+      continue;
     }
 
-    console.log(
-      `Extracted languages to tmp directory, ${languages.length} languages (including English) found.`,
-    );
+    language = language.replace('_', '-');
+    const outLanguagePath = path.join(outDir, language);
+    console.log(`Importing language ${language}...`);
 
-    languages.forEach((language) => {
-      const languagePath = path.join(tmpDir, language);
-      if (!fs.statSync(languagePath).isDirectory() || language === 'en') {
-        return;
-      }
+    await fs.promises.mkdir(outLanguagePath, { recursive: true });
 
-      fs.readdir(languagePath, (err, files) => {
-        if (err) {
-          console.error(err);
-          exit(1);
-        }
+    const files = await fs.promises.readdir(languagePath);
+    const filePromises = files.map(async (file) => {
+      const source = path.join(languagePath, file);
+      const target = path.join(outLanguagePath, file.replace('.lokalise.json', '.ts'));
 
-        language = language.replace('_', '-');
-        const outLanguagePath = path.join(outDir, language);
-        console.log(`Importing language ${language}...`);
+      const namespace = file.split('.')[0];
+      const name = namespace === 'translation' ? 'translation' : 'languageInfo';
+      const type = namespace === 'translation' ? 'I18nTranslation' : 'I18nLangInfoTranslation';
 
-        fs.mkdirSync(outLanguagePath, { recursive: true });
+      const translationObject = await generateTranslationObject(source, lokaliseTranslation[namespace]);
+      const sortedTranslationObject = sortedJSONify(translationObject);
+      const code = `// ATTENTION: This is an auto-generated file. Do not edit this file manually.
+        
+          import type { ${type} } from '../models';
 
-        files.forEach(async (file) => {
-          const source = path.join(languagePath, file);
-          const target = path.join(outLanguagePath, file.replace('.lokalise.json', '.ts'));
-
-          const namespace = file.split('.')[0];
-          const name = namespace === 'translation' ? 'translation' : 'languageInfo';
-          const type = namespace === 'translation' ? 'I18nTranslation' : 'I18nLangInfoTranslation';
-
-          const translationObject = generateTranslationObject(source);
-          const code = `// ATTENTION: This is an auto-generated file. Do not edit this file manually.
+          const ${name}: ${type} = ${sortedTranslationObject};
           
-            import type { ${type} } from '../models';
+          export default ${name};
+        `;
 
-            const ${name}: ${type} = ${sortedJSONify(translationObject)};
-            
-            export default ${name};
-          `;
-
-          const translationContent = await prettier.format(code, {
-            parser: 'typescript',
-            ...prettierConfig,
-          });
-
-          fs.writeFileSync(target, translationContent);
-        });
+      const translationContent = await prettier.format(code, {
+        parser: 'typescript',
+        ...prettierConfig,
       });
-    });
-  });
-};
 
-if (require.main === module) {
-  importFromLokalise();
+      return fs.promises.writeFile(target, translationContent);
+    });
+
+    await Promise.all(filePromises);
+  }
 }
+
+importFromLokalise();
