6d72d997f71d41ea171f404e98ae3d65ab219cde
Hatem Hosny <hatemhosny@gmail.com>
2024-09-16 00:38:26 +0300

    do not save appLanguage if embed or temporarily set in params

--- src/livecodes/core.ts
+++ src/livecodes/core.ts
@@ -4160,6 +4160,10 @@ const translateStringMock = <Key extends I18nKeyType, Value extends string>(
 };
 
 const setAppLanguage = (reload: boolean = false) => {
+  const lang = i18n?.getLanguage() ?? 'en';
+  document.documentElement.lang = lang;
+  if (isEmbed || params.appLanguage) return;
+
   const flatten = (obj: I18nTranslationTemplate, prefix = ''): { [k: string]: string } =>
     Object.keys(obj).reduce((acc, key) => {
       const value = obj[key];
@@ -4169,11 +4173,9 @@ const setAppLanguage = (reload: boolean = false) => {
       return { ...acc, [`${prefix}${key}`]: value };
     }, {});
 
-  const lang = i18n?.getLanguage() ?? 'en';
   const i18nSplashData =
     !isEmbed && i18n ? flatten(i18n.translateKey('splash', { returnObjects: true })) : {};
 
-  document.documentElement.lang = lang;
   parent.postMessage(
     {
       args: 'i18n',
