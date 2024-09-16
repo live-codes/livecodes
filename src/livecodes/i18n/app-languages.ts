b184baee4ed041cbea12a8a2eb81453aeab8e77e
zyf722 <MaxMixAlex@protonmail.com>
2024-09-16 10:22:34 +0800

    fix(i18n): temporary disable Arabic

--- src/livecodes/i18n/app-languages.ts
+++ src/livecodes/i18n/app-languages.ts
@@ -3,5 +3,5 @@ import type { AppLanguage } from '../models';
 export const appLanguages: { [key in Exclude<AppLanguage, 'auto'>]: string } = {
   en: 'English',
   'zh-CN': '中文（简体）',
-  ar: 'العربية',
+  // ar: 'العربية',
 };
