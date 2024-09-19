da72fd16eaf687e20b992d8300f30ea6de9023c3
Hatem Hosny <hatemhosny@gmail.com>
2024-09-19 21:33:31 +0300

    fix code-to-img styles

--- src/livecodes/UI/code-to-image.ts
+++ src/livecodes/UI/code-to-image.ts
@@ -150,6 +150,7 @@ export const createCodeToImageUI = async ({
       setTimeout(() => {
         // wait till the editor is rendered
         formData.width = (backgroundEl.offsetWidth / backgroundEl.parentElement!.offsetWidth) * 100;
+
         form[`code-to-img-width`].value = formData.width;
 
         edirtorContainer.classList.toggle('shadow', Boolean(formData.shadow));
