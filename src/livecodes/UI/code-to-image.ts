93e5c1c27c0f6a41f4332c30ca5ad45cf99eb3c0
Hatem Hosny <hatemhosny@gmail.com>
2024-09-19 21:42:49 +0300

    share url

--- src/livecodes/UI/code-to-image.ts
+++ src/livecodes/UI/code-to-image.ts
@@ -361,7 +361,6 @@ export const createCodeToImageUI = async ({
       setTimeout(() => {
         // wait till the editor is rendered
         formData.width = (backgroundEl.offsetWidth / backgroundEl.parentElement!.offsetWidth) * 100;
-
         form[`code-to-img-width`].value = formData.width;
       }, 150);
     } else {
