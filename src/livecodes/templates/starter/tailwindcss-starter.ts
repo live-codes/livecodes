6f2f30cfb71bef44525085e9a69ee8efea2fe6e5
Hatem Hosny <hatemhosny@gmail.com>
2025-01-30 07:40:44 +0200

    update tailwindcss starter template

--- src/livecodes/templates/starter/tailwindcss-starter.ts
+++ src/livecodes/templates/starter/tailwindcss-starter.ts
@@ -8,61 +8,77 @@ export const tailwindcssStarter: Template = {
   markup: {
     language: 'html',
     content: `
-<div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-    <div class="back-card"></div>
-    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-      <div class="max-w-md mx-auto">
-        <div>
-          <img src="{{ __livecodes_baseUrl__ }}assets/templates/tailwindplay.svg" class="h-7 sm:h-8" />
-        </div>
-        <div class="divide-y divide-gray-200">
-          <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-            <p class="prose md:prose-xl">A template based on <a href="https://play.tailwindcss.com/" class="text-cyan-600 hover:text-cyan-700" target="_blank">Tailwind CSS playground</a>. Here you can do things like:</p>
-            <ul class="list-disc space-y-2">
-              <li class="flex items-start">
-                <span class="h-6 flex items-center sm:h-7">
-                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                  </svg>
-                </span>
-                <p class="ml-2">
-                  Customizing configuration in
-                  <code class="text-sm font-bold text-gray-900">menu → Custom Settings</code>
-                </p>
-              </li>
-              <li class="flex items-start">
-                <span class="h-6 flex items-center sm:h-7">
-                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                  </svg>
-                </span>
-                <p class="ml-2">
-                  Extracting classes with
-                  <code class="text-sm font-bold text-gray-900">@apply</code>
-                </p>
-              </li>
-              <li class="flex items-start">
-                <span class="h-6 flex items-center sm:h-7">
-                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                  </svg>
-                </span>
-                <p class="ml-2">Viewing generated CSS code (in <code class="text-sm font-bold text-gray-900">Compiled</code> pane below)</p>
-              </li>
-            </ul>
-            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
-          </div>
-          <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
-            <p>Want to dig deeper into Tailwind?</p>
-            <p>
-              <a href="https://tailwindcss.com/docs" class="text-cyan-600 hover:text-cyan-700" target="_blank"> Read the docs &rarr; </a>
+<div class="relative grid min-h-screen grid-cols-[1fr_2.5rem_auto_2.5rem_1fr] grid-rows-[1fr_1px_auto_1px_1fr] bg-white [--pattern-fg:var(--color-gray-950)]/5 dark:bg-gray-950 dark:[--pattern-fg:var(--color-white)]/10">
+  <div class="col-start-3 row-start-3 flex max-w-lg flex-col bg-gray-100 p-2 dark:bg-white/10">
+    <div class="rounded-xl bg-white p-10 text-sm/7 text-gray-700 dark:bg-gray-950 dark:text-gray-300">
+      <img src="{{ __livecodes_baseUrl__ }}assets/templates/tailwind-play.svg" class="mb-11.5 h-6 dark:hidden" alt="Tailwind Play" />
+      <img src="{{ __livecodes_baseUrl__ }}assets/templates/tailwind-play-dark.svg" class="mb-11.5 h-6 not-dark:hidden" alt="Tailwind Play" />
+      <div class="space-y-6">
+        <p>A template based on <a href="https://play.tailwindcss.com/" class="text-cyan-600 hover:text-cyan-700" target="_blank">Tailwind CSS playground</a>, including support for things like:</p>
+        <ul class="space-y-3">
+          <li class="flex items-center">
+            <svg class="size-5.5 shrink-0" fill="none" stroke-linecap="square">
+              <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
+              <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
+              <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+            </svg>
+            <p class="ml-3">
+              Customizing your theme with
+              <code class="font-mono font-medium text-gray-950 dark:text-white">@theme</code>
             </p>
-          </div>
-        </div>
+          </li>
+          <li class="flex items-center">
+            <svg class="size-5.5 shrink-0" fill="none" stroke-linecap="square">
+              <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
+              <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
+              <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+            </svg>
+            <p class="ml-3">
+              Adding custom utilities with
+              <code class="font-mono font-medium text-gray-950 dark:text-white">@utility</code>
+            </p>
+          </li>
+          <li class="flex items-center">
+            <svg class="size-5.5 shrink-0" fill="none" stroke-linecap="square">
+              <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
+              <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
+              <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+            </svg>
+            <p class="ml-3">
+              Adding custom variants with
+              <code class="font-mono font-medium text-gray-950 dark:text-white">@variant</code>
+            </p>
+          </li>
+          <li class="flex items-center">
+            <svg class="size-5.5 shrink-0" fill="none" stroke-linecap="square">
+              <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
+              <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
+              <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+            </svg>
+            <p class="ml-3">Code completion with instant preview</p>
+          </li>
+          <li class="flex items-center">
+            <svg class="size-5.5 shrink-0" fill="none" stroke-linecap="square">
+              <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
+              <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
+              <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+            </svg>
+            <p class="ml-3">Viewing generated CSS code (in <code class="text-sm font-bold text-gray-900">Compiled</code> pane below)</p>
+          </li>
+        </ul>
+        <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
       </div>
+      <hr class="my-6 w-full border-(--pattern-fg)" />
+      <p class="mb-3">Want to dig deeper into Tailwind?</p>
+      <p class="font-semibold">
+        <a href="https://tailwindcss.com/docs" class="text-gray-950 underline decoration-sky-400 underline-offset-3 hover:decoration-2 dark:text-white">Read the docs &rarr;</a>
+      </p>
     </div>
   </div>
+  <div class="relative -right-px col-start-2 row-span-full row-start-1 border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed"></div>
+  <div class="relative -left-px col-start-4 row-span-full row-start-1 border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed"></div>
+  <div class="relative -bottom-px col-span-full col-start-1 row-start-2 h-px bg-(--pattern-fg)"></div>
+  <div class="relative -top-px col-span-full col-start-1 row-start-4 h-px bg-(--pattern-fg)"></div>
 </div>
 `.trimStart(),
   },
@@ -71,8 +87,8 @@ export const tailwindcssStarter: Template = {
     content: `
 @import "tailwindcss";
 
-.back-card {
-  @apply absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl;
+@theme {
+  /* ... */
 }
 `.trimStart(),
   },
