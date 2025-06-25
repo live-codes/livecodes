5e4ea43af86f1ba32be40ef4e5ab97128947b3bd
Hatem Hosny <hatemhosny@gmail.com>
2025-06-26 02:17:22 +0300

    wip

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -6,6 +6,3 @@ if expr "${HOST_NAME}" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >
 fi
 
 $@
-
-# this file line endings has to be unix (LF)
-#https://stackoverflow.com/questions/26671815/not-foundsh-2-error-comes-while-exectutin-a-shell-command
