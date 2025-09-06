0c1e45f540d83bc2115d00b8bda05e8533f2e7d2
Hatem Hosny <hatemhosny@gmail.com>
2025-09-06 21:32:34 +0300

    allow changing ripple compiler version

---
+++ src/livecodes/languages/ripple/lang-ripple-compiler.ts
@@ -0,0 +1,46 @@
+import type { CompilerFunction, Config } from '../../models';
+import { modulesService } from '../../services/modules';
+
+(self as any).createRippleCompiler = async (initialConfig: Config): Promise<CompilerFunction> => {
+  let version = initialConfig.customSettings.ripple?.version || 'latest';
+  let compile: (code: string, filename: string) => Promise<{ js: { code: string }; css: string }>;
+
+  const updateCompiler = async (currentVersion: string) => {
+    if (typeof compile === 'function' && currentVersion === version) return;
+    const mod = await import(
+      /* @__PURE__ */ modulesService.getModuleUrl(`ripple@${currentVersion}/compiler`)
+    );
+    compile = mod.compile;
+    version = currentVersion;
+  };
+
+  return async (code, { config }) => {
+    if (!code.trim()) return '';
+
+    const newVersion = config.customSettings.ripple?.version || version;
+    await updateCompiler(newVersion);
+
+    const { js, css } = await compile(code, './App.ripple');
+
+    const cssCode =
+      css === ''
+        ? ''
+        : `\n
+const styles = document.createElement('style');
+styles.innerHTML = ${JSON.stringify(css)};
+document.head.appendChild(styles);
+`;
+
+    const moduleUrl = /* @__PURE__ */ modulesService.getModuleUrl(`ripple@${version}`);
+    return {
+      code: `${js.code}${cssCode}`,
+      info: {
+        imports: {
+          ripple: moduleUrl,
+          'ripple/internal': `${moduleUrl}/internal`,
+          'ripple/internal/client': `${moduleUrl}/internal/client`,
+        },
+      },
+    };
+  };
+};
