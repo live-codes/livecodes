abd3e58263040db37936b9d18ca16937f27da64d
Hatem Hosny <hatemhosny@gmail.com>
2025-09-06 20:42:36 +0300

    allow importing external ripple components

--- src/livecodes/languages/ripple/lang-ripple-compiler.ts
+++ src/livecodes/languages/ripple/lang-ripple-compiler.ts
@@ -1,11 +1,22 @@
+import { createImportMap, getCompileResult, replaceSFCImports } from '../../compiler';
 import { compileBlocks } from '../../compiler/compile-blocks';
 import type { CompilerFunction, Config } from '../../models';
 import { modulesService } from '../../services/modules';
+import { getLanguageByAlias } from '../utils';
 
-// TODO: recursively compile imported Ripple components
+const getLatestVersion = async () => {
+  const pkg = await fetch('https://data.jsdelivr.com/v1/packages/npm/ripple')
+    .then((res) => res.json())
+    .catch(() => ({}));
+  return pkg.tags?.latest || 'latest';
+};
 
 (self as any).createRippleCompiler = async (initialConfig: Config): Promise<CompilerFunction> => {
-  let version = initialConfig.customSettings.ripple?.version || 'latest';
+  const MAIN_FILE = '__LiveCodes_App__.ripple';
+  let importedContent = '';
+  let imports: Record<string, string> = {};
+
+  let version: string = initialConfig.customSettings.ripple?.version || (await getLatestVersion());
   let compile: (code: string, filename: string) => Promise<{ js: { code: string }; css: string }>;
 
   const updateCompiler = async (currentVersion: string) => {
@@ -17,35 +28,67 @@ import { modulesService } from '../../services/modules';
     version = currentVersion;
   };
 
-  return async (code, { config }) => {
-    if (!code.trim()) return '';
+  const compileRipple = async (
+    code: string,
+    { config, filename }: { config: Config; filename: string },
+  ) => {
+    if (filename === MAIN_FILE) {
+      importedContent = '';
+      imports = {};
+    }
+    if (!code.trim()) return getCompileResult('');
 
     const newVersion = config.customSettings.ripple?.version || version;
     await updateCompiler(newVersion);
 
-    code = await compileBlocks(code, 'style', config);
+    const isRipple = (mod: string) =>
+      mod.toLowerCase().endsWith('.ripple') || mod.toLowerCase().startsWith('data:text/ripple');
 
-    const { js, css } = await compile(code, './App.ripple');
+    const fullCode = await replaceSFCImports(code, {
+      config,
+      filename,
+      getLanguageByAlias,
+      isSfc: isRipple,
+      compileSFC: async (
+        code: string,
+        { config, filename }: { config: Config; filename: string },
+      ) => {
+        const compiled = (await compileRipple(code, { config, filename })).code;
+        importedContent += `\n${filename}\n\n${code}\n`;
+        return compiled;
+      },
+    });
+    const processedCode = await compileBlocks(fullCode, 'style', config);
+    const { js, css } = await compile(processedCode, filename);
 
     const cssCode =
       css === ''
         ? ''
-        : `\n
+        : `
 const styles = document.createElement('style');
 styles.innerHTML = ${JSON.stringify(css)};
 document.head.appendChild(styles);
 `;
 
-    const moduleUrl = /* @__PURE__ */ modulesService.getModuleUrl(`ripple@${version}`);
+    if (filename === MAIN_FILE) {
+      const moduleUrl = /* @__PURE__ */ modulesService.getModuleUrl(`ripple@${version}`);
+      imports = {
+        ...createImportMap(importedContent, config),
+        ripple: moduleUrl,
+        'ripple/internal': `${moduleUrl}/internal`,
+        'ripple/internal/client': `${moduleUrl}/internal/client`,
+      };
+    }
+
     return {
-      code: `${js.code}${cssCode}`,
-      info: {
-        imports: {
-          ripple: moduleUrl,
-          'ripple/internal': `${moduleUrl}/internal`,
-          'ripple/internal/client': `${moduleUrl}/internal/client`,
-        },
-      },
+      code: `${js.code}\n${cssCode}`,
+      info: { importedContent, imports },
     };
   };
+
+  return (code, { config }) =>
+    compileRipple(code, {
+      config,
+      filename: MAIN_FILE,
+    });
 };
