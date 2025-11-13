8be3f2bfc9d037c57ec8908c194435329b44cde5
rainflower12 <544739517m@gmail.com>
2025-11-14 05:03:10 +0800

    style: format code according to Prettier

--- src/sdk/reveal.ts
+++ src/sdk/reveal.ts
@@ -1,55 +1,58 @@
 import type Reveal from 'reveal.js';
-import type { EmbedOptions, Playground } from './models';
 import { createPlayground } from './index';
+// eslint-disable-next-line import/order
+import type { EmbedOptions, Playground } from './models';
 
 export interface LiveOptions extends EmbedOptions {
-    sdkReady?: (sdk: Playground) => void;
+  sdkReady?: (sdk: Playground) => void;
 }
 
 export interface GlobalLiveCodesOptions extends Reveal.Options {
-    livecodes?: LiveOptions;
-    customStyle?: Partial<CSSStyleDeclaration>;
+  livecodes?: LiveOptions;
+  customStyle?: Partial<CSSStyleDeclaration>;
 }
 
 export interface LiveCodesInstance {
-    id: string;
-    init(deck: InstanceType<typeof Reveal>): void;
+  id: string;
+  init(deck: InstanceType<typeof Reveal>): void;
 }
 
 const initIframeStyle = (iframe: HTMLIFrameElement, styles: Partial<CSSStyleDeclaration>) => {
-    for (const [key, value] of Object.entries(styles)) {
-        // @ts-ignore
-        iframe.style[key as any] = value;
-    }
+  for (const [key, value] of Object.entries(styles)) {
+    // @ts-ignore
+    iframe.style[key as any] = value;
+  }
 };
 
 export const LiveCodes = {
-    id: "LiveCodes",
-    init(deck: InstanceType<typeof Reveal>) {
-        const ContainerList = document.querySelectorAll<HTMLElement>("[data-livecodes]");
-        if (ContainerList.length < 1) {
-            return;
-        }
-        const containers = Array.from(ContainerList);
-        const config = deck.getConfig() as GlobalLiveCodesOptions;
-        const globalOptions = config.livecodes || {};
-        const sdkReadyfn = config.livecodes?.sdkReady;
-        const customStyle = config.customStyle || {};
-        const promises = containers.map((container) => {
-            const localOptions = container.dataset.config || "{}";
-            const finalOptions = { config: { ...globalOptions, ...JSON.parse(localOptions) } }
-            return createPlayground(container, finalOptions);
-        });
-        Promise.all(promises).then((sdk) => {
-            const iframes = document.querySelectorAll<HTMLIFrameElement>('.livecodes');
-            iframes.forEach(iframe => initIframeStyle(iframe, { maxWidth: "100%", maxHeight: "100%", ...customStyle }));
-            if (typeof sdkReadyfn === 'function') {
-                sdk.forEach((itemSdk) => sdkReadyfn(itemSdk));
-            }
-        });
-    },
+  id: 'LiveCodes',
+  init(deck: InstanceType<typeof Reveal>) {
+    const ContainerList = document.querySelectorAll<HTMLElement>('[data-livecodes]');
+    if (ContainerList.length < 1) {
+      return;
+    }
+    const containers = Array.from(ContainerList);
+    const config = deck.getConfig() as GlobalLiveCodesOptions;
+    const globalOptions = config.livecodes || {};
+    const sdkReadyfn = config.livecodes?.sdkReady;
+    const customStyle = config.customStyle || {};
+    const promises = containers.map((container) => {
+      const localOptions = container.dataset.config || '{}';
+      const finalOptions = { config: { ...globalOptions, ...JSON.parse(localOptions) } };
+      return createPlayground(container, finalOptions);
+    });
+    Promise.all(promises).then((sdk) => {
+      const iframes = document.querySelectorAll<HTMLIFrameElement>('.livecodes');
+      iframes.forEach((iframe) =>
+        initIframeStyle(iframe, { maxWidth: '100%', maxHeight: '100%', ...customStyle }),
+      );
+      if (typeof sdkReadyfn === 'function') {
+        sdk.forEach((itemSdk) => sdkReadyfn(itemSdk));
+      }
+    });
+  },
 };
 
 if (typeof window !== 'undefined') {
-    (window as any).LiveCodes = LiveCodes;
+  (window as any).LiveCodes = LiveCodes;
 }
