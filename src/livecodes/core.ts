496260a50d23b31a75864b109a90cfe4594de295
KaiHikichi <kai.hikichi@gmail.com>
2026-09-09 18:41:40 -0700

    updated title

--- src/livecodes/core.ts
+++ src/livecodes/core.ts
@@ -1194,7 +1194,7 @@ const setWindowTitle = () => {
       : '';
 
   parent.document.title =
-    hostLabel + (title && title !== 'Untitled Project' ? title + ' - ' : '') + 'LiveCodes';
+    hostLabel + (title && title !== 'Untitled Project' ? title + ' - ' : '') + 'Wattsons-Playground';
 };
 
 const setExternalResourcesMark = () => {
